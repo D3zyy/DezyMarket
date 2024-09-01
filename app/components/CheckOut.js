8c378de022b7fbeac8df50c947ca5a87f8195a72
D3zyy <michaldolezel@gmail.com>
2024-09-01 13:41:05 +0200

    working payments

--- app/components/CheckOut.js
+++ app/components/CheckOut.js
@@ -55,7 +55,7 @@ const handleSubmit = async (event) => {
         elements,
         clientSecret,
         confirmParams:{
-            
+            return_url: 'http://localhost:3000/addPost'
         },
     })
     if (!errorPayment) {
@@ -70,7 +70,7 @@ const handleSubmit = async (event) => {
 if(!clientSecret || !stripe || !elements)
 {
     return <div style={{textAlign: "center"}}>  <div
-    class="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
+    className="inline-block h-8 w-8 animate-spin rounded-full border-4 border-solid border-current border-e-transparent align-[-0.125em] text-surface motion-reduce:animate-[spin_1.5s_linear_infinite] dark:text-white"
     role="status" >
   </div></div>
 }
