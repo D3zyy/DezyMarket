10155f6f7e08de2155ddd9708a8bf0ff3dcf065c
D3zyy <michaldolezel@gmail.com>
2024-09-27 17:00:24 +0200

    working file image uploading

--- app/addPost/AddUI.js
+++ app/addPost/AddUI.js
@@ -239,35 +239,90 @@ const AddUI = ({ accType, userCategories }) => {
       {/* Image input */}
       <label >
       <input
-        style={{display: "none"}}
-        type="file"
-        aria-label='yo'
-        title='Title'
-        accept="image/*"
-        multiple
-        onChange={handleImageChange}
-        className="mb-4"
-        disabled={
-            images.length >= (
-              typeOfPost === process.env.NEXT_PUBLIC_BASE_RANK
-                ? 2
-                : typeOfPost === process.env.NEXT_PUBLIC_MEDIUM_RANK
-                ? 5
-                : typeOfPost === process.env.NEXT_PUBLIC_BEST_RANK
-                ? 10
-                : 2
-            )
-          }
-      />
-            <div className="flex items-center mb-2 ">
-  <a className="flex items-center" style={{border: "2px dotted gray", borderRadius: "2%", padding: "20px"}}>
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 mr-1">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-    </svg>
-    Nahrát obrázky
-  </a>
-  
-</div>
+      style={{ display: "none" }}
+      type="file"
+      aria-label="yo"
+      title="Title"
+      accept="image/*"
+      multiple
+      onChange={handleImageChange}
+      className="mb-4"
+      disabled={
+        images.length >= (
+          typeOfPost === process.env.NEXT_PUBLIC_BASE_RANK
+            ? 2
+            : typeOfPost === process.env.NEXT_PUBLIC_MEDIUM_RANK
+            ? 5
+            : typeOfPost === process.env.NEXT_PUBLIC_BEST_RANK
+            ? 10
+            : 2
+        )
+      }
+    />
+    <div className="flex flex-col items-center justify-center mb-2 relative border-2 border-dotted border-gray-500 rounded-lg p-4">
+      {/* Blur effect when input is disabled */}
+      {images.length >= (
+        typeOfPost === process.env.NEXT_PUBLIC_BASE_RANK
+          ? 2
+          : typeOfPost === process.env.NEXT_PUBLIC_MEDIUM_RANK
+          ? 5
+          : typeOfPost === process.env.NEXT_PUBLIC_BEST_RANK
+          ? 10
+          : 2
+      ) && (
+        <div className="absolute inset-0 backdrop-blur-sm rounded-lg" />
+      )}
+      
+      {/* Display the count above the blur when the limit is reached */}
+      {images.length >= (
+        typeOfPost === process.env.NEXT_PUBLIC_BASE_RANK
+          ? 2
+          : typeOfPost === process.env.NEXT_PUBLIC_MEDIUM_RANK
+          ? 5
+          : typeOfPost === process.env.NEXT_PUBLIC_BEST_RANK
+          ? 10
+          : 2
+      ) ? (
+        <div className="flex items-center justify-center mb-1 relative z-10">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 mr-2"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+        </svg>
+        <span className="text-gray-500 text-center">
+          Obrázky  {images.length}/{typeOfPost === process.env.NEXT_PUBLIC_BASE_RANK ? 2 : typeOfPost === process.env.NEXT_PUBLIC_MEDIUM_RANK ? 5 : 10}
+        </span>
+    </div>
+      ) : (
+        // "Nahrát obrázky" text when the limit is not reached
+        <div className="flex items-center justify-center mb-1 relative z-10">
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 mr-2"
+        >
+            <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="m2.25 15.75 5.159-5.159a2.25 2.25 0 0 1 3.182 0l5.159 5.159m-1.5-1.5 1.409-1.409a2.25 2.25 0 0 1 3.182 0l2.909 2.909m-18 3.75h16.5a1.5 1.5 0 0 0 1.5-1.5V6a1.5 1.5 0 0 0-1.5-1.5H3.75A1.5 1.5 0 0 0 2.25 6v12a1.5 1.5 0 0 0 1.5 1.5Zm10.5-11.25h.008v.008h-.008V8.25Zm.375 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Z"
+            />
+        </svg>
+        <span className="text-center text-gray-500 relative z-10">Nahrát obrázky</span>
+        </div>
+      )}
+    </div>
     </label>
       {/* Image preview */}
       <div className="flex flex-wrap gap-4 justify-center items-center">
