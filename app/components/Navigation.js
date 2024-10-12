b47ad8560c80ea02bfd5d91a008616fb05e31b9e
D3zyy <michaldolezel@gmail.com>
2024-10-12 11:56:42 +0200

    fixed fonts not wokring

--- app/components/Navigation.js
+++ app/components/Navigation.js
@@ -1,35 +1,29 @@
 import React from 'react'
 import Link from 'next/link'
 import AuthenticateUser from './AuthenticateUser'
-import { Pacifico} from 'next/font/google';
+import { Pacifico } from 'next/font/google';
 import { Bebas_Neue } from 'next/font/google'
 
+// Import and assign the fonts correctly
+const pacifico = Pacifico({ subsets: ['latin'], weight: '400' });
+const bebas = Bebas_Neue({ subsets: ['latin'], weight: '400' });
 
-const babes = Bebas_Neue({
-    subsets: ['latin'],
-    variable: '--font-babes',
-    weight: '400',
-    display: 'swap'
-  }) 
-
-const pacifico = Pacifico({
-  subsets: ['latin'],
-  variable: '--font-pacifico',
-  weight: '400',
-  display: 'swap'
-})
-
-const Navigation = async () => {
-
+const Navigation = () => {
   return (
     <div className="navbar bg-base-100">
-          <div className="flex-1">
-          <Link href="/"className={`${babes.className} btn btn-ghost text-xl`}style={{ fontSize: "1.5rem" }}>Dezy</Link>
-          <span  className={pacifico.className} style={{ marginLeft: "5px"}}>„když chci něco víc.“</span> 
-          </div>
-          <div>
-            <AuthenticateUser />
-          </div>
+      <div className="flex-1">
+        {/* Apply Bebas Neue font to the Link with larger font size */}
+        <Link href="/" className={`${bebas.className} btn btn-ghost`} style={{ fontSize: "1.8rem" }}>
+          Dezy
+        </Link>
+        {/* Apply Pacifico font to the slogan with larger font size */}
+        <span className={pacifico.className} style={{ marginLeft: "10px", fontSize: "1rem" }}>
+          „když chci něco víc.“
+        </span>
+      </div>
+      <div>
+        <AuthenticateUser />
+      </div>
     </div>
   )
 }
