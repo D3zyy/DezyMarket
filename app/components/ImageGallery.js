b5ab645141a05a0787254fb398a64053c8e25af1
D3zyy <michaldolezel@gmail.com>
2024-10-26 22:35:23 +0200

    updated back working last photo

--- app/components/ImageGallery.js
+++ app/components/ImageGallery.js
@@ -24,16 +24,7 @@ const ImageGallery = ({ allImages }) => {
       url: 'https://loremflickr.com/1200/1200?random=2',
       postId: 11
     },
-    {
-      id: 7,
-      url: 'https://loremflickr.com/300/300?random=1',
-      postId: 11
-    },
-    {
-      id: 8,
-      url: 'https://loremflickr.com/400/400?random=2',
-      postId: 11
-    }
+    
   ];
 
 
@@ -92,25 +83,32 @@ const ImageGallery = ({ allImages }) => {
               </div>
             ))}
 
-            {/* Last thumbnail with dynamic overlay if more than 4 images */}
-            {additionalCount > 0 && (
-              <div className="relative w-full h-20">
-                <div
-                  className="absolute inset-0 rounded-lg bg-opacity-25 cursor-pointer"
-                  onClick={() => setIsGalleryOpen(true)} // Open gallery
-                >
-                  <img
-                    src={allImages[3].url}
-                    alt="Thumbnail 4"
-                    className="w-full h-full rounded-lg"
-                  />
-                  <div className="flex items-center justify-center absolute inset-0 bg-black bg-opacity-50 text-white font-bold rounded-lg">
-                    +{additionalCount}
-                  </div>
+             {/* Last thumbnail with dynamic "+x" overlay if there are more than 4 images */}
+        {additionalCount >= 0 && (
+          <div className="relative w-full h-20">
+            <div 
+              className="absolute inset-0 rounded-lg bg-opacity-25 cursor-pointer"
+              onClick={() => setIsGalleryOpen(true)} // Open gallery modal
+            >
+              <img
+                src={allImages[3].url} 
+                alt="Thumbnail 4"
+                className="w-full h-full rounded-lg"
+              />
+              {additionalCount > 0 && additionalCount >= 1 ? (
+                <div className="flex items-center justify-center absolute inset-0 bg-black bg-opacity-50 text-white font-bold rounded-lg">
+                  +{additionalCount}
                 </div>
-              </div>
-            )}
+              ) : <div className="flex items-center justify-center absolute inset-0 bg-black bg-opacity-50 text-white font-bold rounded-lg">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+            </svg>
+
+            </div>}
+            </div>
           </div>
+        )}
+      </div>
 
           {/* Gallery Modal */}
           {isGalleryOpen && (
