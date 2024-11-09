f0ebb78cdef1d61b4477b9a80eac013e9d9144cb
D3zyy <michaldolezel@gmail.com>
2024-11-09 18:47:26 +0100

    added breadcrubs not responsible on mobile yet

--- app/postt/[postId]/page.js
+++ app/postt/[postId]/page.js
@@ -56,7 +56,34 @@ const Page = async ({ params }) => {
   return (
   <div>
     
+    <div className="pr-8b pl-8 breadcrumbs text-sm">
+  <ul>
+    <li>
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="h-4 w-4  ">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m2.25 12 8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25" />
+</svg>
 
+       
+        <Link className="ml-2" href={"/"}>
+          Domů
+        </Link>
+    </li>
+    <li>
+        
+      <Link href={`/category?id=${postRecord?.categoryId}`}>
+          <span className="mr-2" dangerouslySetInnerHTML={{ __html: postRecord?.category?.logo }}></span>
+          <span className="">{postRecord?.category?.name}</span>
+        </Link>
+    </li>
+    <li>
+     
+        <Link href={`/category?categoryId=${postRecord?.categoryId}&sectionId=${postRecord?.sectionId}`}>
+          {postRecord?.section?.name}
+        </Link>
+
+    </li>
+  </ul>
+</div>
     <div
     className="flex flex-col lg:flex-row p-4 lg:p-8"
     style={{ boxShadow: '0 0 10px var(--fallback-p,oklch(var(--p)/var(--tw-bg-opacity)))' }}
