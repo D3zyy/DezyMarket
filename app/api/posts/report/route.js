ffb3a8537ce0a06779e1aa089ec075c50fa29bc2
D3zyy <michaldolezel@gmail.com>
2024-11-11 18:12:14 +0100

    fully working report modal on post

--- app/api/posts/report/route.js
+++ app/api/posts/report/route.js
@@ -1,13 +1,19 @@
 import { getSession } from "@/app/authentication/actions";
 import { prisma } from "@/app/database/db";
 
+function sanitizeInput(input) {
+  const unsafeChars = /[<>;()&|]/g; // Add any other characters you want to block
+  return input.replace(unsafeChars, ''); // Remove unsafe characters
+}
+
 export async function POST(req) {
     try {
       const session = await getSession();
     
       if (!session || !session.isLoggedIn || !session.email) {
         return new Response(JSON.stringify({
-          message: "Chyba na serveru [POST] požadavek na report příspěvku. Session nebyla nalezena "
+          message: "Chyba na serveru [POST] požadavek na report příspěvku. Session nebyla nalezena ",
+          success: false
         }), {
           status: 400,
           headers: { 'Content-Type': 'application/json' }
@@ -24,18 +30,31 @@ export async function POST(req) {
     ];
       const data = await req.json();
       console.log("Received data report POST:", data);
-
-      const invalidReasons = data.reasons.filter(reason => !reasons.includes(reason));
-
-      if (invalidReasons.length > 0) {
-       // console.log("Nesprávné důvody:", invalidReasons);
+      if (data.reasons.length < 1) {
         return new Response(JSON.stringify({
-            message: "Nějaký z důvodů není platný"
+            message: "Žádný z důvodů nebyl nalezen",
+            success: false
           }), {
             status: 403,
             headers: { 'Content-Type': 'application/json' }
           });
       } 
+      // Sanitize input to remove unsafe characters
+      data.reasons = data.reasons.map(sanitizeInput);
+      data.extraInfo = sanitizeInput(data.extraInfo);
+
+      const invalidReasons = data.reasons.filter(reason => !reasons.includes(reason));
+
+      if (invalidReasons.length > 0) {
+        return new Response(JSON.stringify({
+            message: "Nějaký z důvodů není platný",
+            success: false
+          }), {
+            status: 403,
+            headers: { 'Content-Type': 'application/json' }
+          });
+      } 
+
       // Fetch the post and the creator's role
       const post = await prisma.posts.findUnique({
         where: { id: data.postId },
@@ -44,63 +63,62 @@ export async function POST(req) {
   
       if (!post) {
         return new Response(JSON.stringify({
-          message: "Příspěvek nenalezen"
+          message: "Příspěvek nenalezen",
+          success: false
         }), {
           status: 404,
           headers: { 'Content-Type': 'application/json' }
         });
       }
   
-  
       // If the session user is the post creator
       if (post.userId === session.userId) {      
         return new Response(JSON.stringify({
-            message: "Nelze nahlásit vlastnní příspěvek"
+            message: "Nelze nahlásit vlastnní příspěvek",
+            success: false
           }), {
             status: 403,
             headers: { 'Content-Type': 'application/json' }
           });
       }
-      console.log(session)
+
       const alreadyReported = await prisma.postReport.findFirst({
         where: {
           postId: data.postId,
           userId: session.userId,  // assuming session.userId contains the user's ID
         },
       });
-       
-      
-      console.log("Již reportoval :",alreadyReported)
-      if(alreadyReported){
+
+      if (alreadyReported) {
         return new Response(JSON.stringify({
-            message: 'Příspěvek byl již vámi nahlášen'
+            message: 'Příspěvek jste již nahlásili',
+            success: false
           }), {
             status: 403,
             headers: { 'Content-Type': 'application/json' }
           });
       }
-           
-            let insertedTopic = false;
+      
+      let insertedTopic = false;
 
-            for (const reason of data.reasons) {
-     
-            await prisma.postReport.create({
-                data: {
+      for (const reason of data.reasons) {
+        await prisma.postReport.create({
+            data: {
                 postId: data.postId,
                 userId: session.userId,
                 reason: reason,
                 topic: !insertedTopic && data.extraInfo ? data.extraInfo : null,
-                },
-            });
+            },
+        });
 
-            if (data.extraInfo && !insertedTopic) {
-                insertedTopic = true;
-            }
-            }
-
+        if (data.extraInfo && !insertedTopic) {
+            insertedTopic = true;
+        }
+      }
 
       return new Response(JSON.stringify({
-        message: 'Příspěvek byl úspěšně nahlášen'
+        message: 'Příspěvek byl úspěšně nahlášen',
+        success: true
       }), {
         status: 200,
         headers: { 'Content-Type': 'application/json' }
@@ -109,10 +127,90 @@ export async function POST(req) {
     } catch (error) {
       console.error('Chyba na serveru [POST] požadavek na report příspěvku: ', error);
       return new Response(JSON.stringify({
-        message: 'Chyba na serveru [POST] požadavek  na smazání příspěvku'
+        message: 'Chyba na serveru [POST] požadavek na nahlášení příspěvku',
+        success: false
       }), {
         status: 500,
         headers: { 'Content-Type': 'application/json' }
       });
     }
-  }
+}
+
+
+
+
+
+
+
+
+
+
+
+export async function PUT(req) {
+    try {
+      const session = await getSession();
+    
+      if (!session || !session.isLoggedIn || !session.email) {
+        return new Response(JSON.stringify({
+          message: "Chyba na serveru [PUT] požadavek na zjištění zda byl příspěvek uživatel již nahlášen:. Session nebyla nalezena ",
+          success: false
+        }), {
+          status: 400,
+          headers: { 'Content-Type': 'application/json' }
+        });
+      }
+      
+      const data = await req.json();
+      console.log("Received data to check reported post on user PUT:", data);
+    
+      
+
+      // Fetch the post and the creator's role
+      const post = await prisma.posts.findUnique({
+        where: { id: data.postId },
+        include: { user: { include: { role: true } } }  // Include the user and their role
+      });
+  
+      if (!post) {
+        return new Response(JSON.stringify({
+          message: "Příspěvek nenalezen",
+          success: false
+        }), {
+          status: 404,
+          headers: { 'Content-Type': 'application/json' }
+        });
+      }
+  
+      // If the session user is the post creator check whether the user has already reported the post
+
+        const alreadyReported = await prisma.postReport.findMany({
+            where: {
+              postId: data.postId,
+              userId: session.userId,  // assuming session.userId contains the user's ID
+            },
+          });
+          console.log("Již reportnul:",alreadyReported)
+          return new Response(JSON.stringify({
+           reported: alreadyReported,
+           
+          }), {
+            status: 200,
+            headers: { 'Content-Type': 'application/json' }
+          });
+      
+     
+    
+     
+
+
+    } catch (error) {
+      console.error('Chyba na serveru [PUT] požadavek na zjištění zda byl příspěvek uživatel již nahlášen: ', error);
+      return new Response(JSON.stringify({
+        message: 'Chyba na serveru [PUT] požadavek na zjištění zda byl příspěvek uživatel již nahlášen:',
+        success: false
+      }), {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' }
+      });
+    }
+}
--- app/components/modals/ReportPostModal.js
+++ app/components/modals/ReportPostModal.js
@@ -36,11 +36,40 @@ export const ReportPostModal = ({ post }) => {
     const router = useRouter();
     const [loading, setLoading] = useState(false);
     const [success, setSuccess] = useState(false);
+    const [errorFromServer, setErrorFromServer] = useState(false); 
     const [postId, setPostId] = useState(post?.id);
     const [selectedReasons, setSelectedReasons] = useState([]);
     const [moreInfo, setMoreInfo] = useState('');
     const [isDropdownOpen, setDropdownOpen] = useState(false);
-
+    const [errorMessage, setErrorMessage] = useState('');
+    const [alreadyReported, setAlreadyReported] = useState('');
+    useEffect(() => {
+        const checkReportStatus = async () => {
+            try {
+                
+                const response = await fetch('/api/posts/report', {
+                    method: 'PUT', 
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
+                    body: JSON.stringify({ postId }),
+                });
+    
+                if (response.ok) {
+                    const data = await response.json();
+                    console.log("data:",data)
+                    setAlreadyReported(data.reported);
+                    console.log(alreadyReported)
+                } else {
+                    console.error('Failed to report post:', response.statusText);
+                }
+            } catch (error) {
+                console.error('Error checking report status:', error);
+            }
+        };
+    
+        checkReportStatus();
+    }, [postId]); // Add dependencies here if needed
     const reasons = [
         'Nesprávná kategorie',
         'Nesprávná sekce',
@@ -60,8 +89,21 @@ export const ReportPostModal = ({ post }) => {
     };
 
     const handleReportChange = async () => {
+        if (selectedReasons.length === 0) {
+            setErrorMessage('Důvod je povinný.');
+            return; // Prevent submission if no reason is selected
+        }
+
+        setErrorMessage(''); // Clear error message if validation passes
         setLoading(true);
-        await reportPost(postId, selectedReasons, setSuccess, moreInfo);
+       let resultsFromReport =  await reportPost(postId, selectedReasons, setSuccess, moreInfo);
+       console.log("co server odpovedel:",resultsFromReport)
+       if(resultsFromReport.success){
+        setSuccess(resultsFromReport.message)
+       } else{
+        setErrorFromServer(true)
+        setErrorMessage(resultsFromReport.message)
+       }
         setLoading(false);
     };
 
@@ -88,89 +130,144 @@ export const ReportPostModal = ({ post }) => {
                         />
                     </svg>
                 </div>
+                {errorMessage && (
+  <div className="flex items-center gap-2 px-4 py-2  border border-red-500 rounded-md text-red-600 font-semibold mt-2">
+    <span>{errorMessage}</span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+    </svg>
+  </div>
+)}
+
+{success && (
+  <div className="flex items-center gap-2 px-4 py-2  border border-green-500 rounded-md text-green-600 font-semibold mt-2">
+    <span>{success}</span>
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      fill="none"
+      viewBox="0 0 24 24"
+      strokeWidth={1.5}
+      stroke="currentColor"
+      className="w-6 h-6"
+    >
+      <path strokeLinecap="round" strokeLinejoin="round" d="m4.5 12.75 6 6 9-13.5" />
+    </svg>
+  </div>
+)}
+
+
                 <div className="w-full p-4 rounded-lg">
                     <div className="w-full  relative mx-auto">
+                        {alreadyReported.length >0 && (
+ <div role="alert" className="alert alert-warning mb-5 p-2">
+ <svg
+   xmlns="http://www.w3.org/2000/svg"
+   className="h-9 w-9 shrink-0 stroke-current"
+   fill="none"
+   viewBox="0 0 24 24">
+   <path
+     strokeLinecap="round"
+     strokeLinejoin="round"
+     strokeWidth="2"
+     d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z" />
+ </svg>
+ <span>Již nahlášeno</span>
+</div>
+)}
                         <button
+                          
                             className="select select-bordered w-full !inline-block " // Light primary background with transparency
                             onClick={() => setDropdownOpen(!isDropdownOpen)}
                         >
                             Zvolte důvod/y
                         </button>
                         {isDropdownOpen && (
-                            <div className="absolute bg-base-200 rounded-md top-full left-0 w-full mt-1 z-10">
-                                {reasons.map((reason) => (
-                                    <label key={reason} className="flex items-center cursor-pointer p-2">
-                                        <input
-                                            type="checkbox"
-                                            checked={selectedReasons.includes(reason)}
-                                            onChange={() => handleReasonToggle(reason)}
-                                            className="checkbox mr-2"
-                                        />
-                                        {reason}
-                                    </label>
-                                ))}
-                                
+                            <div className="absolute bg-base-300 rounded-md top-full left-0 w-full mt-1 z-10">
+                               {reasons.map((reason) => {
+    const isAlreadyReported = alreadyReported.some((report) => report.reason === reason);
+
+    return (
+        <label key={reason} className="flex items-center cursor-pointer p-2">
+            <input
+                disabled={isAlreadyReported || success}
+                type="checkbox"
+                checked={selectedReasons.includes(reason) || isAlreadyReported}
+                onChange={() => handleReasonToggle(reason)}
+                className="checkbox mr-2"
+            />
+            {reason}
+        </label>
+    );
+})}
                             </div>
                         )}
                     </div>
                     <div className="w-full mt-5 relative mx-auto">
-                        <textarea
-             
-                        style={{ 
-                            fontSize: '14px', 
-                            padding: '8px', 
-                            height: '150px', // Počáteční výška
-                            resize: 'none' // Zamezení změny velikosti
-                          }}
-                            name="moreInfo"
-                            id="moreInfo"
-                            value={moreInfo}
-                            onChange={(e) => setMoreInfo(e.target.value)}
-                            className="input input-bordered w-full  text-left" // Keep input text left-aligned
-                            placeholder="Řekněte nám víc.." // Placeholder text
-                        />
+                    <textarea
+    disabled={alreadyReported || success}
+    maxLength="230"
+    style={{ 
+        fontSize: '14px', 
+        padding: '8px', 
+        height: '150px', // Initial height
+        resize: 'none' // Prevent resizing
+    }}
+    name="moreInfo"
+    id="moreInfo"
+    value={alreadyReported[0]?.topic || moreInfo} // Use topic if available, otherwise moreInfo
+    onChange={(e) => setMoreInfo(e.target.value)}
+    className="input input-bordered w-full text-left" // Keep input text left-aligned
+    placeholder="Řekněte nám víc.." // Placeholder text
+/>
                     </div>
                     <div className="bg-base-200 collapse mt-5 mx-auto">
-  <input type="checkbox" className="peer " />
-  <div className="collapse-title bg-base-200 peer-checked:bg-base-300 flex items-center ">
-  Chci nahlásit uživatele
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
-  </svg>
-</div>
-  <div
-    className="collapse-content bg-base-300  peer-checked:bg-base-300">
-<p>
-  Nahlášení uživatele je možné na jeho profilu{" "}
-  <Link className='underline' target="_blank" href={`/user/${post?.user?.id}`}>{post?.user?.fullName}</Link>
-</p>
-  </div>
-</div>
-<div className="bg-base-200 collapse mt-5 mx-auto">
-  <input type="checkbox" className="peer " />
-  <div className="collapse-title bg-base-200 peer-checked:bg-base-300 flex items-center ">
-  Byl jsem podveden
-  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
-    <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
-  </svg>
-</div>
-  <div
-    className="collapse-content bg-base-300  peer-checked:bg-base-300">
-<p>
-Nahlašte podvod na Policii ČR. V případě, že budete potřebovat dodatečné informace, kontaktujte nás prostřednictvím <Link className='underline' target="_blank" href={"/support"}>Podpory</Link>
-</p>
-  </div>
-</div>
-
+                        <input type="checkbox" className="peer " />
+                        <div className="collapse-title bg-base-200 peer-checked:bg-base-300 flex items-center ">
+                            Chci nahlásit uživatele
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+                            </svg>
+                        </div>
+                        <div className="collapse-content bg-base-300  peer-checked:bg-base-300">
+                            <p>
+                                Nahlášení uživatele je možné na jeho profilu{" "}
+                                <Link className='underline' target="_blank" href={`/user/${post?.user?.id}`}>{post?.user?.fullName}</Link>
+                            </p>
+                        </div>
+                    </div>
+                    <div className="bg-base-200 collapse mt-5 mx-auto">
+                        <input type="checkbox" className="peer " />
+                        <div className="collapse-title bg-base-200 peer-checked:bg-base-300 flex items-center ">
+                            Byl jsem podveden
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 ml-2">
+                                <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+                            </svg>
+                        </div>
+                        <div className="collapse-content bg-base-300  peer-checked:bg-base-300">
+                            <p>
+                                Nahlašte podvod na Policii ČR. V případě, že budete potřebovat dodatečné informace, kontaktujte nás prostřednictvím <Link className='underline' target="_blank" href={"/support"}>Podpory</Link>
+                            </p>
+                        </div>
+                    </div>
                 </div>
+               
                 <div className="flex mt-4 justify-center w-full">
-                    <button
+                    {!errorFromServer && !success && !alreadyReported.length >0 ?  <button
                         onClick={handleReportChange}
                         className="btn mr-2 hover:bg-red-600 bg-red-500"
                         disabled={loading}
+                        
                     >
                         {loading && !success ? 'Načítání...' : "Nahlásit příspěvek"}
-                    </button>
+                    </button>: ""}
+                   
                     <button
                         onClick={closeReportPostModal}
                         className="btn"
