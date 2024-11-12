243d1365ef77af3459907bde3d5275bd6230087f
D3zyy <michaldolezel@gmail.com>
2024-11-12 18:21:26 +0100

    added laoding spinner to report modal on post

--- app/components/modals/ReportPostModal.js
+++ app/components/modals/ReportPostModal.js
@@ -43,34 +43,40 @@ export const ReportPostModal = ({ post }) => {
     const [isDropdownOpen, setDropdownOpen] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
     const [alreadyReported, setAlreadyReported] = useState('');
+    const [loadingStatus, setLoadingStatus] = useState(true); // Track loading status
     const dropdownRef = useRef(null);  // Reference to the dropdown container
 
     useEffect(() => {
-        const checkReportStatus = async () => {
-            try {
-                const response = await fetch('/api/posts/report', {
-                    method: 'PUT', 
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    body: JSON.stringify({ postId }),
-                });
+    const checkReportStatus = async () => {
+        setLoadingStatus(true); // Zobrazí spinner
 
-                if (response.ok) {
-                    const data = await response.json();
-                    console.log("data:", data)
-                    setAlreadyReported(data.reported);
-                    console.log(alreadyReported)
-                } else {
-                    console.error('Failed to report post:', response.statusText);
-                }
-            } catch (error) {
-                console.error('Error checking report status:', error);
+        try {
+            const response = await fetch('/api/posts/report', {
+                method: 'PUT', 
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({ postId }),
+            });
+
+            if (response.ok) {
+                const data = await response.json();
+                setAlreadyReported(data.reported);
+            } else {
+                console.error('Failed to report post:', response.statusText);
             }
-        };
+        } catch (error) {
+            console.error('Error checking report status:', error);
+        }
 
-        checkReportStatus();
-    }, [postId]); // Add dependencies here if needed
+        // Nastaví zpoždění, aby spinner byl zobrazen alespoň 2 sekundy
+        setTimeout(() => {
+            setLoadingStatus(false); // Skrývá spinner po zpoždění
+        }, 2000); // Zpoždění 2 sekundy (2000 ms)
+    };
+
+    checkReportStatus();
+}, [postId]);
 
     // Close dropdown if clicking outside
     useEffect(() => {
@@ -135,59 +141,66 @@ export const ReportPostModal = ({ post }) => {
             style={{ marginLeft: "0px" }}
         >
             <div className="modal-box w-full p-6 flex flex-col items-center text-center">
+            {loadingStatus ?   <span className="loading loading-spinner loading-lg"></span>  :  <>
                 <div className="flex justify-center mb-4">
-                    {!success && !alreadyReported.length > 0 ? (
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            viewBox="0 0 24 24"
-                            strokeWidth={1.5}
-                            stroke="currentColor"
-                            className="size-16 text-red-600"
-                        >
-                            <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"
-                            />
-                        </svg>
-                    ) : (
+                  
+                      
+                   
                         <>
-                            {success && (
-                                <div role="alert" className="flex flex-col items-center text-green-500 mb-2 p-3  rounded-lg ">
-                                    <svg 
-                                        xmlns="http://www.w3.org/2000/svg" 
-                                        fill="none" 
-                                        viewBox="0 0 24 24" 
-                                        strokeWidth={1.5} 
-                                        stroke="currentColor" 
-                                        className="size-16 mb-2"
-                                    >
-                                        <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
-                                    </svg>
-                                    <span className="text-sm font-medium text-center">{success}</span>
-                                </div>
-                            )}
-                            {alreadyReported.length > 0 && (
-                                <div role="alert" className="text-yellow-500 mb-2 p-3">
-                                    <svg
-                                        xmlns="http://www.w3.org/2000/svg"
-                                        className="size-16 mx-auto mb-2 stroke-current"
-                                        fill="none"
-                                        viewBox="0 0 24 24"
-                                    >
-                                        <path
-                                            strokeLinecap="round"
-                                            strokeLinejoin="round"
-                                            strokeWidth="2"
-                                            d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
-                                        />
-                                    </svg>
-                                    <span className="text-sm font-medium block text-center">Již nahlášeno</span>
-                                </div>
+                            {!success && !alreadyReported.length > 0 ? (
+                                <svg
+                                    xmlns="http://www.w3.org/2000/svg"
+                                    fill="none"
+                                    viewBox="0 0 24 24"
+                                    strokeWidth={1.5}
+                                    stroke="currentColor"
+                                    className="size-16 text-red-600"
+                                >
+                                    <path
+                                        strokeLinecap="round"
+                                        strokeLinejoin="round"
+                                        d="M3 3v1.5M3 21v-6m0 0 2.77-.693a9 9 0 0 1 6.208.682l.108.054a9 9 0 0 0 6.086.71l3.114-.732a48.524 48.524 0 0 1-.005-10.499l-3.11.732a9 9 0 0 1-6.085-.711l-.108-.054a9 9 0 0 0-6.208-.682L3 4.5M3 15V4.5"
+                                    />
+                                </svg>
+                            ) : (
+                                <>
+                                    {success && (
+                                        <div role="alert" className="flex flex-col items-center text-green-500 mb-2 p-3  rounded-lg ">
+                                            <svg 
+                                                xmlns="http://www.w3.org/2000/svg" 
+                                                fill="none" 
+                                                viewBox="0 0 24 24" 
+                                                strokeWidth={1.5} 
+                                                stroke="currentColor" 
+                                                className="size-16 mb-2"
+                                            >
+                                                <path strokeLinecap="round" strokeLinejoin="round" d="M4.5 12.75l6 6 9-13.5" />
+                                            </svg>
+                                            <span className="text-sm font-medium text-center">{success}</span>
+                                        </div>
+                                    )}
+                                    {alreadyReported.length > 0 && (
+                                        <div role="alert" className="text-yellow-500 mb-2 p-3">
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                className="size-16 mx-auto mb-2 stroke-current"
+                                                fill="none"
+                                                viewBox="0 0 24 24"
+                                            >
+                                                <path
+                                                    strokeLinecap="round"
+                                                    strokeLinejoin="round"
+                                                    strokeWidth="2"
+                                                    d="M12 9v2m0 4h.01m-6.938 4h13.856c1.54 0 2.502-1.667 1.732-3L13.732 4c-.77-1.333-2.694-1.333-3.464 0L3.34 16c-.77 1.333.192 3 1.732 3z"
+                                                />
+                                            </svg>
+                                            <span className="text-sm font-medium block text-center">Již nahlášeno</span>
+                                        </div>
+                                    )}
+                                </>
                             )}
                         </>
-                    )}
+            
                 </div>
 
                 {errorMessage && (
@@ -256,27 +269,28 @@ export const ReportPostModal = ({ post }) => {
                     </div>
 
                     <div className="modal-action mt-6 flex justify-center w-full">
-                    {!errorFromServer && !success && !alreadyReported.length >0 ?  <button
-                            onClick={handleReportChange}
-                            className="btn btn-primary  "
-                            disabled={loading || selectedReasons.length === 0 || success || alreadyReported.length > 0}
-                            
+                        {!errorFromServer && !success && !alreadyReported.length > 0 ? (
+                            <button
+                                onClick={handleReportChange}
+                                className="btn btn-primary  "
+                                disabled={loading || selectedReasons.length === 0 || success || alreadyReported.length > 0}
+                            >
+                                {loading && !success ? 'Odesílám...' : "Nahlásit"}
+                            </button>
+                        ) : ""}
+
+                        <button
+                            onClick={closeReportPostModal}
+                            className="btn"
+                            autoFocus
+                            disabled={loading}
                         >
-                            {loading && !success ? 'Odesílám...' : "Nahlásit"}
-                        </button>: ""}
-                       
-                    
-                    
-                    <button
-                        onClick={closeReportPostModal}
-                        className="btn"
-                        autoFocus
-                        disabled={loading}
-                    >
-                        Zavřít
-                    </button>
+                            Zavřít
+                        </button>
                     </div>
                 </div>
+                     </>  
+}
             </div>
         </dialog>
     );
