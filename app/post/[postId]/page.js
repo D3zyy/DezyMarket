538250d5a378ebbd142b63c5a380296bf55a6de5
D3zyy <michaldolezel@gmail.com>
2024-12-03 13:56:51 +0100

    updated admin menu layout on post page

--- app/post/[postId]/page.js
+++ app/post/[postId]/page.js
@@ -101,11 +101,14 @@ const Page = async ({ params }) => {
     {postRecord?.name}
     </li>
   </ul>
+  
 </div>
+
     <div
     className="flex flex-col lg:flex-row p-4 lg:p-8"
     style={{ boxShadow: '0 0 10px var(--fallback-p,oklch(var(--p)/var(--tw-bg-opacity)))' }}
   >
+    
      <ImageGallery  allImages={imageUrls} typeOfPost={postRecord.typeOfPost}/> 
     
 
@@ -114,6 +117,62 @@ const Page = async ({ params }) => {
       {/* Right section - Information */}
       
       <div className="lg:w-1/2   min-h-[700px] flex flex-col justify-between lg:pl-8">
+      {session?.role?.privileges > postRecord?.user?.role?.privileges && 
+<div className="flex space-x-4 mt-4 mb-4">
+  <a 
+    className="btn border-dotted border-red-600 border-2 flex-shrink hover:border-red-600" 
+    onClick={openEditPostModal}
+  >
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      fill="none" 
+      viewBox="0 0 24 24" 
+      strokeWidth={1.5} 
+      stroke="currentColor" 
+      className="size-6 text-red-600"
+    >
+      <path 
+        strokeLinecap="round" 
+        strokeLinejoin="round" 
+        d="m16.862 4.487 1.687-1.688a1.875 1.875 0 1 1 2.652 2.652L10.582 16.07a4.5 4.5 0 0 1-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 0 1 1.13-1.897l8.932-8.931Zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0 1 15.75 21H5.25A2.25 2.25 0 0 1 3 18.75V8.25A2.25 2.25 0 0 1 5.25 6H10" 
+      />
+    </svg>
+  </a>
+  
+
+  <a 
+    className="btn border-dotted border-red-600 border-2 flex-shrink hover:border-red-600" 
+    onClick={openDeletePostModal}
+  >
+    <svg 
+      xmlns="http://www.w3.org/2000/svg" 
+      fill="none" 
+      viewBox="0 0 24 24" 
+      strokeWidth={1.5} 
+      stroke="currentColor" 
+      className="size-6 text-red-600"
+    >
+      <path 
+        strokeLinecap="round" 
+        strokeLinejoin="round" 
+        d="M6 18 18 6M6 6l12 12" 
+      />
+    </svg>
+  </a>
+  <a 
+    className="btn border-dotted border-yellow-500 border-2 flex-shrink hover:border-yellow-500" 
+    onClick={openDeletePostModal}
+  >
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-yellow-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+</svg>
+
+
+
+  
+  </a>
+</div>
+}
       {!(postRecord.typeOfPost === process.env.BASE_RANK) && (
   <div
   className="mt-4 lg:mt-0 badge badge-md badge-outline mb-5"
@@ -434,23 +493,7 @@ Ohodnotit uživatele</a>
 </div></> }
 
 </div>
-{session?.role?.privileges > postRecord?.user?.role?.privileges && 
-<div className="flex items-center justify-center space-x-4  mt-4 border border-dashed  border-red-500 p-4  rounded-md">
-<a className="btn sm:h-0 h-20 flex-shrink" onClick={openEditPostModal} style={{ color: "red", borderColor: "red", borderWidth: "1px" }}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-        </svg> 
-        Upravit příspěvek
-      </a>
 
-      <a className="btn sm:h-0 h-20 flex-shrink" onClick={openDeletePostModal} style={{ color: "red", borderColor: "red", borderWidth: "1px" }}>
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-          <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
-        </svg> 
-        Smazat příspěvek
-      </a>
-  </div>
-}
       </div>
 
     </div>
