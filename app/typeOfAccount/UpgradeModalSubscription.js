7a3a09b91f69180d6306f1de0ae4409b6c981d92
D3zyy <michaldolezel@gmail.com>
2024-12-13 19:50:52 +0100

    updated upperCase

--- app/typeOfAccount/UpgradeModalSubscription.js
+++ app/typeOfAccount/UpgradeModalSubscription.js
@@ -180,7 +180,7 @@ export function UpgradeModalSubscription({ nameToUpgrade, date }) {
                         d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z"
                     />
                 </svg>
-                {`**** ${card.last4}  ${card.brand}`}
+                {`**** ${card.last4}  ${card.brand.toUpperCase()}`}
             </option>
         ))}
     </select>
