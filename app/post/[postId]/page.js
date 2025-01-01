4f666a6a7be3edb593f0d4d399669b889e096943
D3zyy <michaldolezel@gmail.com>
2025-01-01 18:14:18 +0100

    updated view count button on post page

--- app/post/[postId]/page.js
+++ app/post/[postId]/page.js
@@ -179,11 +179,10 @@ const Page = async ({ params }) => {
   {postRecord?.name}
 </h1>
 
-            <div className="btn inline-block w-32 mb-5" style={{ display: 'flex', alignItems: 'center' }}>
-           <span className="text-3xl">&#128064;</span>
-  <span>200x</span>
+<div className="btn inline-block w-32 mb-5 " style={{ display: 'flex', alignItems: 'center' }}>
+  <span className="text-3xl" style={{ marginTop: '-4px' }}>&#128064;</span>
+  <span className="font-bold">200x</span>
 </div>
-
  
   
 
