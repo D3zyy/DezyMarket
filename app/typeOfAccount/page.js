26a638f57c1b2d27093edb2dec4a0b01e9ffc5c2
D3zyy <michaldolezel@gmail.com>
2025-01-06 13:49:28 +0100

    updated design

--- app/typeOfAccount/page.js
+++ app/typeOfAccount/page.js
@@ -197,16 +197,32 @@ const emoji3 = `<div class='badge badge-outline'>${process.env.BASE_RANK}</div>`
       ) : null}
 
 
-     <h3 className='mt-5 mb-2 font-bold italic '
+<h3
+  className="mt-5 mb-2 font-bold italic"
   style={{
     textAlign: "center",
     fontSize: "large",
-    
   }}
 >
-Topování
-
-
+  
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-9"
+    style={{
+      display: "inline-block",
+      verticalAlign: "middle",
+    }}
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 0 1 3 19.875v-6.75ZM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V8.625ZM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 0 1-1.125-1.125V4.125Z"
+    />
+  </svg>
 </h3>
 
 <h3 className='font-normal mb-3 flex justify-center items-center'>
@@ -232,10 +248,10 @@ Topování
       ))
     ) : (
       <>
-        <span className='mr-3'>Dostupné pro: </span>
+       
         {sortedAcctypes?.map((accType) => (
           accType.priority !== 1 ? (
-            <span key={accType.name + accType.priority} className='font-normal text-center mr-2' dangerouslySetInnerHTML={{ __html: accType.emoji }}></span>
+            <span key={accType.name + accType.priority} className='font-normal text-center mr-2 mt-2' dangerouslySetInnerHTML={{ __html: accType.emoji }}></span>
           ) : null
         ))}
       </>
