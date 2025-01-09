fe9b8fce6f5d99e8768c85e1a78f996ddf8dc48a
D3zyy <michaldolezel@gmail.com>
2025-01-09 17:25:00 +0100

    updated subscsitpipn security on create-subs..

--- app/api/create-subscription/route.js
+++ app/api/create-subscription/route.js
@@ -1,7 +1,8 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getSession } from "@/app/authentication/actions";
 import { custom } from "zod";
-
+import { prisma } from "@/app/database/db";
+import { DateTime } from "luxon";
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 export async function POST(request) {
@@ -20,8 +21,90 @@ export async function POST(request) {
 
    
        const { priceId , agreed, nameOfSub} = await request.json()
-  
+ 
+ 
+       const accTohaveExist = await prisma.accountType.findFirst({ // or find him where userId =session.userId
+        where: { name:nameOfSub, priority: {
+          gt: 1, 
+        }, },
+    });
+
+    if (!accTohaveExist) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Účet který byl poslán že chcete neexistuje' }),
+        {
+          status: 403,
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+    }
+
+    const existWithuser = await prisma.AccountTypeUsers.findFirst({
+      where: {
+          active: true,
+          userId: session.userId,
+          accountType: {
+              id : accTohaveExist.id  , 
+               priority: {
+                gt: 1, 
+              },
+          }
+      },
+    });
+
+
+    if (existWithuser) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Účet který byl poslán že chcete již máte.' }),
+        {
+          status: 403,
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+    }
       
+
+    const dateAndTime = DateTime.now()
+    .setZone('Europe/Prague')
+    .toFormat("yyyy-MM-dd'T'HH:mm:ss'+00:00'");
+  
+  // Hledání aktuálně platné ceny podle priceCode
+  const priceToBuy = await prisma.accountTypeOnPrices.findFirst({
+    where: {
+      id: accTohaveExist.id,
+      activeFrom: {
+        lte: dateAndTime, // Musí být platné od minulosti nebo teď
+      },
+      OR: [
+        {
+          activeTo: null, // Platí do neurčita
+        },
+        {
+          activeTo: {
+            gte: dateAndTime, // Musí být stále platné (budoucnost nebo teď)
+          },
+        },
+      ],
+      price: {
+        priceCode: priceId, // Podmínka na propojený priceCode z tabulky prices
+      },
+    },
+    include: {
+      price: true, // Zahrnutí detailů ceny
+    },
+  });
+
+  if (!priceToBuy) {
+    return new NextResponse(
+      JSON.stringify({ message: 'Zadaná cena není aktuálně platná pro tento účet.' }),
+      {
+        status: 404,
+        headers: { 'Content-Type': 'application/json' },
+      }
+    );
+  }
+
+
 
        const customer = await stripe.customers.search({
         query: `email:'${session.email}'`,
@@ -29,37 +112,7 @@ export async function POST(request) {
       
         
         
-        const subscriptions = await stripe.subscriptions.list({
-          customer: customer.id,
-        
-          status: "active",
-          expand: ['data.items.data.price'], // Rozbalí objekt price pro kontrolu ID
-      });
-      const subscriptionsAll = await stripe.subscriptions.list({
-        customer: customer.id,
-        expand: ['data.items.data.price'], // Rozbalí objekt price pro kontrolu ID
-    });
-      const hasExistingSubscription = subscriptions.data.some(subscription =>
-        subscription.items.data.some(item => item.price.id === priceId) // 0 CZK PRICE IN OTHER WORDS THE DEFAULT ACC
-    );
-    if(subscriptionsAll?.length > 10){
-      return new Response(JSON.stringify({
-        message: "Již nalezeno dost předplatných nejspíše se jedná o chybu kontaktujte podporu "
-    }), {
-        status: 403,
-        headers: { 'Content-Type': 'application/json' }
-    });
-    }
-    if (hasExistingSubscription) {
-        return new Response(JSON.stringify({
-            message: "Již nalezeno aktivní předplatné nelze aktivovat základní předplatné."
-        }), {
-            status: 403,
-            headers: { 'Content-Type': 'application/json' }
-        });
-    }
-        console.log("Co vkladam jako name :",nameOfSub)
-        console.log("Price id:",priceId)
+       
         let priceee =priceId
           const subscription = await stripe.subscriptions.create({
             customer:  customer.data[0].id,
