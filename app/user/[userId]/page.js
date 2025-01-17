33cc0e6e9bb549d88bfd79680cbaf9cdda4c7f78
D3zyy <michaldolezel@gmail.com>
2025-01-17 15:31:50 +0100

    adding user page

---
+++ app/user/[userId]/page.js
@@ -0,0 +1,148 @@
+import { getSession } from "@/app/authentication/actions";
+import { prisma } from "@/app/database/db";
+import Link from "next/link";
+
+const Page = async ({ params }) => {
+  const [session, user, rankingOfUser, bansOfUser] = await Promise.all([
+    getSession(),
+    prisma.users.findUnique({
+      where: { id: params?.userId },
+    }),
+    prisma.userRatings.findMany({
+      where: { toUserId: params?.userId },
+      include: {
+        fromUser: true,  // Předpokládám, že vztah mezi userRatings a users je definován jako toUser
+      },
+    }),
+    prisma.bans.findMany({
+      where: { userId: params?.userId },
+    }),
+  ]);
+console.log(rankingOfUser)
+  const formatDate = (date) => {
+    const d = new Date(date);
+    return `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()}`;
+  };
+
+  // Formátování data s tečkami
+  const formattedDate = new Date(user.dateOfRegistration);
+  const day = String(formattedDate.getDate()).padStart(2, '0');
+  const month = String(formattedDate.getMonth() + 1).padStart(2, '0');
+  const year = formattedDate.getFullYear();
+  const formattedDateWithDots = `${day}.${month}.${year}`;
+
+  return (
+    <div className="flex flex-row items-start justify-center space-x-16"> {/* Flexbox pro dvě strany */}
+      
+      {/* Levá strana - Tabulka informací o uživatelském profilu */}
+      <div className="flex flex-col items-center justify-center w-1/3">
+        {/* Uživatelské informace */}
+        <div className="grid grid-cols-2 gap-8 items-center">
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-20 h-20"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M17.982 18.725A7.488 7.488 0 0 0 12 15.75a7.488 7.488 0 0 0-5.982 2.975m11.963 0a9 9 0 1 0-11.963 0m11.963 0A8.966 8.966 0 0 1 12 21a8.966 8.966 0 0 1-5.982-2.275M15 9.75a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+              />
+            </svg>
+          </div>
+          <div className="flex justify-start">
+            <p className="text-lg font-semibold">{user?.fullName}</p>
+          </div>
+
+          {/* Informace o registraci */}
+          <div className="flex justify-center">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 24 24"
+              strokeWidth={1.5}
+              stroke="currentColor"
+              className="w-9 h-9"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                d="M6.75 3v2.25M17.25 3v2.25M3 18.75V7.5a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 7.5v11.25m-18 0A2.25 2.25 0 0 0 5.25 21h13.5A2.25 2.25 0 0 0 21 18.75m-18 0v-7.5A2.25 2.25 0 0 1 5.25 9h13.5A2.25 2.25 0 0 1 21 11.25v7.5m-9-6h.008v.008H12v-.008ZM12 15h.008v.008H12V15Zm0 2.25h.008v.008H12v-.008ZM9.75 15h.008v.008H9.75V15Zm0 2.25h.008v.008H9.75v-.008ZM7.5 15h.008v.008H7.5V15Zm0 2.25h.008v.008H7.5v-.008Zm6.75-4.5h.008v.008h-.008v-.008Zm0 2.25h.008v.008h-.008V15Zm0 2.25h.008v.008h-.008v-.008Zm2.25-4.5h.008v.008H16.5v-.008Zm0 2.25h.008v.008H16.5V15Z"
+              />
+            </svg>
+          </div>
+          <div className="flex justify-start">
+            <p>Registrace {formattedDateWithDots}</p>
+          </div>
+        </div>
+      </div>
+
+      {/* Pravá strana - Sekce pro hodnocení */}
+      <div className="flex flex-col items-start w-3/3">
+        <div className="flex justify-start">
+            <div className="flex flex-row gap-4">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-14 text-yellow-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+</svg>  <p className="text-lg font-semibold">
+Hodnocení uživatele</p>
+            </div>
+        
+        </div>
+
+        {/* Hodnocení uživatele nebo zpráva, pokud žádná nejsou */}
+        {rankingOfUser.length > 0 ? (
+          rankingOfUser.map((rating) => (
+            
+            <div key={rating.id} className="flex flex-col justify-start mb-6 mt-4">
+
+<div className="flex flex-row gap-4 mb-2">
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z" />
+</svg>
+<Link target="_blank" className='underline' href={`/user/${rating.fromUser.id}`}>{rating.fromUser.fullName}</Link>
+    </div>
+
+               
+
+                  <div className="flex flex-row gap-4 mb-2">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6.75 2.994v2.25m10.5-2.25v2.25m-14.252 13.5V7.491a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v11.251m-18 0a2.25 2.25 0 0 0 2.25 2.25h13.5a2.25 2.25 0 0 0 2.25-2.25m-18 0v-7.5a2.25 2.25 0 0 1 2.25-2.25h13.5a2.25 2.25 0 0 1 2.25 2.25v7.5m-6.75-6h2.25m-9 2.25h4.5m.002-2.25h.005v.006H12v-.006Zm-.001 4.5h.006v.006h-.006v-.005Zm-2.25.001h.005v.006H9.75v-.006Zm-2.25 0h.005v.005h-.006v-.005Zm6.75-2.247h.005v.005h-.005v-.005Zm0 2.247h.006v.006h-.006v-.006Zm2.25-2.248h.006V15H16.5v-.005Z" />
+</svg>   <p>
+{formatDate(rating.ratedAt)}</p>
+                    </div>
+           
+              <div className="rating">
+                {[...Array(5)].map((_, index) => (
+                  <input
+                  disabled={true}
+                    key={index}
+                    type="radio"
+                    name={`rating-${rating.id}`}
+                    className="mask mask-star-2 bg-orange-400"
+                    defaultChecked={index < rating.numberOfStars}
+                  />
+                ))}
+              </div>
+        {rating.extraInfo &&    <div className="flex flex-row gap-4 mb-2">
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M8.625 12a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H8.25m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0H12m4.125 0a.375.375 0 1 1-.75 0 .375.375 0 0 1 .75 0Zm0 0h-.375M21 12c0 4.556-4.03 8.25-9 8.25a9.764 9.764 0 0 1-2.555-.337A5.972 5.972 0 0 1 5.41 20.97a5.969 5.969 0 0 1-.474-.065 4.48 4.48 0 0 0 .978-2.025c.09-.457-.133-.901-.467-1.226C3.93 16.178 3 14.189 3 12c0-4.556 4.03-8.25 9-8.25s9 3.694 9 8.25Z" />
+</svg>
+              <p>{rating.extraInfo}</p>
+
+</div>
+}  
+            </div>
+          ))
+        ) : (
+          <p className="text-sm text-gray-500">Tento uživatel nemá žádná hodnocení.</p>
+        )}
+      </div>
+    </div>
+  );
+};
+
+export default Page;
