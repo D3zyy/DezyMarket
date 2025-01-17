4e96c829d126f9d9b63855653ca27a076a8db981
D3zyy <michaldolezel@gmail.com>
2025-01-17 17:05:39 +0100

    updaed

--- app/user/[userId]/page.js
+++ app/user/[userId]/page.js
@@ -1,7 +1,8 @@
 import { getSession } from "@/app/authentication/actions";
 import { prisma } from "@/app/database/db";
 import Link from "next/link";
-
+import { getUserAccountTypeOnStripe } from "@/app/typeOfAccount/Methods";
+import { select } from "@nextui-org/react";
 const Page = async ({ params }) => {
   const [session, posts, rankingOfUser, bansOfUser] = await Promise.all([
     getSession(),
@@ -21,7 +22,16 @@ const Page = async ({ params }) => {
       where: { userId: params?.userId },
     }),
   ]);
-console.log(session)
+   let accType = await getUserAccountTypeOnStripe(session.email);
+
+let emojiForAcc = false
+if(accType?.priority > 1){
+     emojiForAcc = await prisma.AccountType.findMany({
+        where: { name: accType?.name },
+        select: { emoji: true }
+    });
+}
+console.log(emojiForAcc)
   const formatDate = (date) => {
     const d = new Date(date);
     return `${d.getDate()}.${d.getMonth() + 1}.${d.getFullYear()}`;
@@ -88,7 +98,7 @@ console.log(session)
   <path strokeLinecap="round" strokeLinejoin="round" d="M9.813 15.904 9 18.75l-.813-2.846a4.5 4.5 0 0 0-3.09-3.09L2.25 12l2.846-.813a4.5 4.5 0 0 0 3.09-3.09L9 5.25l.813 2.846a4.5 4.5 0 0 0 3.09 3.09L15.75 12l-2.846.813a4.5 4.5 0 0 0-3.09 3.09ZM18.259 8.715 18 9.75l-.259-1.035a3.375 3.375 0 0 0-2.455-2.456L14.25 6l1.036-.259a3.375 3.375 0 0 0 2.455-2.456L18 2.25l.259 1.035a3.375 3.375 0 0 0 2.456 2.456L21.75 6l-1.035.259a3.375 3.375 0 0 0-2.456 2.456ZM16.894 20.567 16.5 21.75l-.394-1.183a2.25 2.25 0 0 0-1.423-1.423L13.5 18.75l1.183-.394a2.25 2.25 0 0 0 1.423-1.423l.394-1.183.394 1.183a2.25 2.25 0 0 0 1.423 1.423l1.183.394-1.183.394a2.25 2.25 0 0 0-1.423 1.423Z" />
 </svg>
 
-        <p>Typ účtu: </p>
+<span dangerouslySetInnerHTML={{ __html: emojiForAcc && emojiForAcc[0] ? emojiForAcc[0].emoji : 'Základní' }} />
         </div>
        
       </div>
