b6023693634f2c0b6e3e3c70bca7ccf6d9a885e6
D3zyy <michaldolezel@gmail.com>
2025-01-19 17:55:41 +0100

    upd

--- app/userr/[userId]/page.js
+++ app/userr/[userId]/page.js
@@ -40,15 +40,46 @@ if(accType?.priority > 1){
 
 
 
-  return (
-    <>
-      <div className='flex justify-center gap-4 p-2'>
-        <div className="border-2 border-dotted  border-gray-600 min-w-full min-h-[450px]  rounded-md">
-
+return (
+  <>
+    <div className="flex justify-center gap-4 p-2">
+      <div className="relative border-2 border-dotted border-gray-900 min-w-full min-h-[450px] rounded-md">
+        {/* Text a SVG zarovnané nad rámeček bez přerušení borderu */}
+        <div className="absolute -top-5 left-1/2 transform -translate-x-1/2  bg-base-100 px-4 flex items-center gap-2">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="w-8 h-8"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 1 1-7.5 0 3.75 3.75 0 0 1 7.5 0ZM4.501 20.118a7.5 7.5 0 0 1 14.998 0A17.933 17.933 0 0 1 12 21.75c-2.676 0-5.216-.584-7.499-1.632Z"
+            />
+          </svg>
+          <span className="font-semibold">{userAcc.fullName}</span>
         </div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="size-12"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M15 9h3.75M15 12h3.75M15 15h3.75M4.5 19.5h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Zm6-10.125a1.875 1.875 0 1 1-3.75 0 1.875 1.875 0 0 1 3.75 0Zm1.294 6.336a6.721 6.721 0 0 1-3.17.789 6.721 6.721 0 0 1-3.168-.789 3.376 3.376 0 0 1 6.338 0Z"
+          />
+        </svg>
       </div>
-    </>
-  )}
+    </div>
+  </>
+)}
 
 
 export default Page;
