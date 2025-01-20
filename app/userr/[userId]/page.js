ab93ca2d3147641840956840a2a909e962517dee
D3zyy <michaldolezel@gmail.com>
2025-01-20 16:03:01 +0100

    upd

--- app/userr/[userId]/page.js
+++ app/userr/[userId]/page.js
@@ -136,23 +136,25 @@ return (
 
 
 
-
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-14 mt-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
+</svg>
       {/* Pravá strana - Sekce pro hodnocení */}
-      <div className="flex   min-w-72 scrollbar-hidden  flex-col justify-center  max-w-48 ">
+      <div className="flex    scrollbar-hidden  flex-col justify-center  items-center ">
         <div className="flex justify-start">
 
         
         
         </div>
 
-        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-14 mt-4">
-  <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
-</svg>
+        
         {/* Hodnocení uživatele nebo zpráva, pokud žádná nejsou */}
-        {rankingOfUser.length > 0 ? (
+        <div className="grid grid-cols-1   sm:grid-cols-2 lg:grid-cols-3 gap-4  "> 
+        {rankingOfUser.length > 0 ? ( 
+      
           rankingOfUser.map((rating) => (
             
-            <div key={rating.id} className="relative flex flex-col justify-start mb-6 mt-4 border-2 dark:border-gray-700 border-gray-300 border-dashed rounded-md p-3">
+            <div key={rating.id} className="relative mt-3 flex flex-col gap-2 max-w-48 break-all border-2 dark:border-gray-700 border-gray-300 border-dashed rounded-md p-3">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute -top-4 left-1/2 transform -translate-x-1/2 bg-base-100 p-1 rounded-full size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M11.48 3.499a.562.562 0 0 1 1.04 0l2.125 5.111a.563.563 0 0 0 .475.345l5.518.442c.499.04.701.663.321.988l-4.204 3.602a.563.563 0 0 0-.182.557l1.285 5.385a.562.562 0 0 1-.84.61l-4.725-2.885a.562.562 0 0 0-.586 0L6.982 20.54a.562.562 0 0 1-.84-.61l1.285-5.386a.562.562 0 0 0-.182-.557l-4.204-3.602a.562.562 0 0 1 .321-.988l5.518-.442a.563.563 0 0 0 .475-.345L11.48 3.5Z" />
 </svg>
@@ -193,11 +195,13 @@ return (
 
 </div>
 }  
+
             </div>
-          ))
+          ) ) 
         ) : (
           <p className="text-sm mt-2 text-gray-500">Tento uživatel nemá žádná hodnocení.</p>
         )}
+        </div>
       </div>
 
 
