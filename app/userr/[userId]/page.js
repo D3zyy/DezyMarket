851c48fbc4807032ba13375219539b3a5e11f77f
D3zyy <michaldolezel@gmail.com>
2025-01-20 16:16:11 +0100

    upd

--- app/userr/[userId]/page.js
+++ app/userr/[userId]/page.js
@@ -140,7 +140,12 @@ return (
   <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 8.511c.884.284 1.5 1.128 1.5 2.097v4.286c0 1.136-.847 2.1-1.98 2.193-.34.027-.68.052-1.02.072v3.091l-3-3c-1.354 0-2.694-.055-4.02-.163a2.115 2.115 0 0 1-.825-.242m9.345-8.334a2.126 2.126 0 0 0-.476-.095 48.64 48.64 0 0 0-8.048 0c-1.131.094-1.976 1.057-1.976 2.192v4.286c0 .837.46 1.58 1.155 1.951m9.345-8.334V6.637c0-1.621-1.152-3.026-2.76-3.235A48.455 48.455 0 0 0 11.25 3c-2.115 0-4.198.137-6.24.402-1.608.209-2.76 1.614-2.76 3.235v6.226c0 1.621 1.152 3.026 2.76 3.235.577.075 1.157.14 1.74.194V21l4.155-4.155" />
 </svg>
       {/* Pravá strana - Sekce pro hodnocení */}
-      <div className="flex    scrollbar-hidden  flex-col justify-center  items-center ">
+      <div
+  className={`flex scrollbar-hidden flex-col justify-center ${
+    rankingOfUser.length > 0 ? "items-center" : ""
+  }`}
+>
+
         <div className="flex justify-start">
 
         
@@ -199,7 +204,7 @@ return (
             </div>
           ) ) 
         ) : (
-          <p className="text-sm mt-2 text-gray-500">Tento uživatel nemá žádná hodnocení.</p>
+          <p className="text-sm mt-2 text-gray-500  whitespace-nowrap ">Tento uživatel nemá žádná hodnocení.</p>
         )}
         </div>
       </div>
@@ -305,7 +310,7 @@ return (
   </div>
   </div>
 ) : (
-  <p className="text-sm mt-2 text-gray-500">
+  <p className="text-sm mt-2 text-gray-500 whitespace-nowrap  ">
     Tento uživatel nemá žádné příspěvky.
   </p>
 )}
