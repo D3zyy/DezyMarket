df81a130285c6a6d3d86fe1d43c585daa6f6f1a1
D3zyy <michaldolezel@gmail.com>
2025-01-21 14:42:22 +0100

    upd

--- app/userr/[userId]/page.js
+++ app/userr/[userId]/page.js
@@ -183,7 +183,7 @@ return (
          {bansOfUser.length > 0 ? (
  bansOfUser.sort((a, b) => new Date(b.bannedFrom) - new Date(a.bannedFrom))
  
- .map((ban) => ( <div key={ban.id} className="relative mt-3 flex flex-col gap-2 max-w-96 break-all border-2 dark:border-gray-700 border-gray-300 border-dashed rounded-md p-3" >
+ .map((ban) => ( <div key={ban.id} className="relative mt-3 flex flex-col gap-2 max-w-96 max-h-[350px] break-all border-2 dark:border-gray-700 border-gray-300 border-dashed rounded-md p-3" >
  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute -top-4 left-1/2 transform -translate-x-1/2 bg-base-100 p-1 rounded-full size-6">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 9v3.75m9-.75a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9 3.75h.008v.008H12v-.008Z" />
 </svg>
@@ -215,7 +215,7 @@ return (
      <div className=" flex flex-row gap-2 max-w-48 break-all">
    
      {ban.reason &&
-       <div className="flex flex-row gap-4">
+       <div className="flex flex-row gap-4  ">
          <svg
            xmlns="http://www.w3.org/2000/svg"
            fill="none"
@@ -230,7 +230,7 @@ return (
              d="M7.5 8.25h9m-9 3H12m-9.75 1.51c0 1.6 1.123 2.994 2.707 3.227 1.129.166 2.27.293 3.423.379.35.026.67.21.865.501L12 21l2.755-4.133a1.14 1.14 0 0 1 .865-.501 48.172 48.172 0 0 0 3.423-.379c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0 0 12 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018Z"
            />
          </svg>
-         <div>
+         <div className="max-h-32 overflow-scroll">
           {ban.reason}
          </div>
          
