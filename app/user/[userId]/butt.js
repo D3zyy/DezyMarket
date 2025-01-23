69dbc4ae1371d3654f93f3df5757831addaeb5a3
D3zyy <michaldolezel@gmail.com>
2025-01-23 17:17:05 +0100

    upd

--- app/user/[userId]/butt.js
+++ app/user/[userId]/butt.js
@@ -4,7 +4,10 @@ import { openAddBanModal } from "./addBanModal";
 export const ButtOp = ({ userId }) => {  // Destructure userId from props
   return (
     <button onClick={() => openAddBanModal()} className="btn btn-sm text-center">
-      Přidat ban
+     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-red-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
+</svg>
+ Ban
     </button>
   );
 };
