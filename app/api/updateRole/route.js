644d444a18afe085bf7493fa0d88f46b92e3bde8
D3zyy <michaldolezel@gmail.com>
2025-01-24 17:41:17 +0100

    upd

---
+++ app/api/updateRole/route.js
@@ -0,0 +1,71 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getSession } from "@/app/authentication/actions";
+import { prisma } from "@/app/database/db";
+import { DateTime } from "luxon"; // Pokud ještě není importováno
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
+export async function POST(request) {
+  try {
+    const session = await getSession();
+    if (!session || !session.isLoggedIn || !session.email) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Chyba na serveru [POST] požadavek na nastavení základní typ účtu. Session nebyla nalezena' }),
+        {
+          status: 500,
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+    }
+    if(session.role.privileges <= 3){
+        return new NextResponse(
+            JSON.stringify({ message: 'Na tento příkaz nemáte oprávnění' }),
+            {
+              status: 403,
+              headers: { 'Content-Type': 'application/json' }
+            }
+          );
+    }
+
+    const { roleId , idOfUser} = await request.json();
+    console.log("roleId:",roleId)
+    console.log("idUserToEdit:",idOfUser)
+    const role = await prisma.roles.findFirst({
+      where: { id: roleId },
+    });
+
+
+    if (!role) {
+      return new NextResponse(
+        JSON.stringify({ message: 'Role nenalezena' }),
+        {
+          status: 404,
+          headers: { 'Content-Type': 'application/json' }
+        }
+      );
+    }
+ await prisma.users.update({
+      where: { id: idOfUser },
+      data : {roleId : roleId }
+    });
+
+    return new NextResponse(
+      JSON.stringify({ message: 'Úspěšná aktualizace role' }),
+      {
+        status: 200,
+        headers: { 'Content-Type': 'application/json' }
+      }
+    );
+
+  } catch (error) {
+    console.error('Chyba při vytváření požadavku na nastavení základního typu předplatného: ', error);
+    return new NextResponse(
+      JSON.stringify({ message: 'Chyba na serveru [POST] požadavek na základní typ předplatného' }),
+      {
+        status: 500,
+        headers: { 'Content-Type': 'application/json' }
+      }
+    );
+  } finally {
+    await prisma.$disconnect(); // Uzavřete připojení po dokončení
+  }
+}
--- app/user/[userId]/UpdRoleSelect.js
+++ app/user/[userId]/UpdRoleSelect.js
@@ -2,16 +2,17 @@
 import React from 'react';
 import { useRouter } from 'next/navigation';
 
-function UpdRoleSelect({ allroles, sessionroleid }) {
+function UpdRoleSelect({ idOfUser,allroles, sessionroleid }) {
   const router = useRouter();
     console.log("All roles:",allroles)
     console.log("Session role id:",sessionroleid)
+    console.log("idOfEditing User:",idOfUser)
   async function updateRole(roleId) {
     try {
       const res = await fetch('/api/updateRole', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ roleId }),
+        body: JSON.stringify({ roleId,idOfUser }),
       });
 
       if (!res.ok) {
