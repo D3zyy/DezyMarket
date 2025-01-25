1b790ff40d76f77b608776e0981f823e0100e743
D3zyy <michaldolezel@gmail.com>
2025-01-25 21:35:52 +0100

    upd

--- app/user/[userId]/page.js
+++ app/user/[userId]/page.js
@@ -283,7 +283,7 @@ return (
 </svg>
  
 
-{(session?.role?.privileges > 2 &&  userAcc?.id != session.userId && accType?.priority <= 1 || session?.role?.privileges > 3 &&accType?.priority <= 1 )&&
+{(session?.role?.privileges > 2 &&  userAcc?.id != session.userId && accType?.priority <= 1 || session?.role?.privileges > 3 &&accType?.priority <= 1||  session?.role?.privileges > 3 &&accType?.priority == null )&&
 <> <button disabled={accType?.priority > 1} onClick={openGiftSubModal} className="btn btn-sm"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 text-green-500">
   <path strokeLinecap="round" strokeLinejoin="round" d="M12 4.5v15m7.5-7.5h-15" />
 </svg>
