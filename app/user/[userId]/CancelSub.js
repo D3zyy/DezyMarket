4a5dace961e5141a761e0f04ad61f3bc3a7da9f4
D3zyy <michaldolezel@gmail.com>
2025-01-25 21:18:44 +0100

    upd

--- app/user/[userId]/CancelSub.js
+++ app/user/[userId]/CancelSub.js
@@ -36,7 +36,10 @@ function CancelSubButton({pri, gifted, name, useToId }) {
       onClick={updateSub}
       disabled={loading || pri <= 1 || pri == null} // Disable the button when loading
     >
-      {loading ? 'Ruším předplatné..' : 'Zrušit předplatné'} {/* Show loading text while request is in progress */}
+      {loading ? 'Ruším předplatné..' : <><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 text-red-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
+</svg> Zrušit předplatné
+ </>} {/* Show loading text while request is in progress */}
     </button>
   );
 }
--- app/user/[userId]/ReNewSub.js
+++ app/user/[userId]/ReNewSub.js
@@ -34,7 +34,10 @@ function ReNewSubButton({ name, useToId }) {
       onClick={updateSub}
       disabled={loading} // Disable the button when loading
     >
-      {loading ? 'Obnovuji předplatné...' : 'Obnovit předplatné'} {/* Show loading text while request is in progress */}
+      {loading ? 'Obnovuji předplatné...' : <><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-4 text-yellow-500">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99" />
+</svg>
+Obnovit předplatné</>} {/* Show loading text while request is in progress */}
     </button>
   );
 }
