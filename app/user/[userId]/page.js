d2f6ba8b66f592e7c086b97cfb232adc38f5c08b
D3zyy <michaldolezel@gmail.com>
2025-01-26 14:32:05 +0100

    upd

--- app/user/[userId]/page.js
+++ app/user/[userId]/page.js
@@ -18,7 +18,7 @@ import ReNewSubButton from "./ReNewSub";
 import DeleteSessionBtn from "./BtnDeleteSession";
 
 const Page = async ({ params }) => {
-  let emojiForAcc,session,userAcc, posts, rankingOfUser, accType,bansOfUser,isBanned,allRoles,allSub,sessionsOfUser,ipsOfUser,ipsWithOtherUsers
+  let emojiForAcc,session,userAcc, posts, rankingOfUser, accType,bansOfUser,isBanned,allRoles,allSub,sessionsOfUser,ipsOfUser,ipsWithOtherUsers,allManagementsActions
   try{
    [session,userAcc, posts, rankingOfUser,bansOfUser] = await Promise.all([
     getSession()
@@ -107,10 +107,8 @@ const Page = async ({ params }) => {
       );
      
 }
-console.log(
-  JSON.stringify(ipsWithOtherUsers, null, 2) // Pretty print with 2 spaces of indentation
-);
-console.log("IP uživatele:",ipsOfUser)
+
+
 if(session?.role?.privileges > 3){
   allRoles = await prisma.roles.findMany({});
   
@@ -226,7 +224,20 @@ if(session?.role?.privileges > 2   ){
 
 isBanned = checkBans(bansOfUser)
 console.log('Is user banned:', isBanned);
+if(session?.role?.privileges > 3 && userAcc?.role?.privileges > 1   ){
+  allManagementsActions = await prisma.managementActions.findMany({
+    where: {
+      fromUserId: userAcc?.id,
+    
+    }, include: {
+      fromUser: true,
+      toUser: true,
+      post: true
 
+    },
+  });
+console.log("Mana act:",allManagementsActions)
+}
 
 
 
@@ -479,6 +490,132 @@ Bude ukončen: {accType?.scheduleToCancel ? 'Ano' : 'Ne'}
 
       </>
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+
+
+
+{(session?.role?.privileges > 3  && userAcc?.role?.privileges > 1) &&  <>
+  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-14 mt-4">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002" />
+</svg>
+
+
+
+
+      {/* Pravá strana - Sekce pro hodnocení */}
+      <div
+     
+  className={`flex scrollbar-hidden flex-col ${ sessionsOfUser.length > 0&&"items-center"} ${
+    sessionsOfUser.length > 0 && sessionsOfUser.length !=1    ? "items-center" : ""
+
+  }`}
+>
+
+      
+
+        
+        {/* Hodnocení uživatele nebo zpráva, pokud žádná nejsou */}
+      <div className={`grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3  gap-4`}>
+        {allManagementsActions.length > 0 ? ( 
+      
+      allManagementsActions .sort((a, b) => new Date(b.doneAt) - new Date(a.doneAt)) .map((action) => (
+        <div key={action.id} className="relative mt-3 flex flex-col gap-2 max-w-96 max-h-[350px] break-all border-2 dark:border-gray-700 border-gray-300 border-dashed rounded-md p-3" >
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="absolute -top-4 left-1/2 transform -translate-x-1/2 bg-base-100 p-1 rounded-full size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M10.05 4.575a1.575 1.575 0 1 0-3.15 0v3m3.15-3v-1.5a1.575 1.575 0 0 1 3.15 0v1.5m-3.15 0 .075 5.925m3.075.75V4.575m0 0a1.575 1.575 0 0 1 3.15 0V15M6.9 7.575a1.575 1.575 0 1 0-3.15 0v8.175a6.75 6.75 0 0 0 6.75 6.75h2.018a5.25 5.25 0 0 0 3.712-1.538l1.732-1.732a5.25 5.25 0 0 0 1.538-3.712l.003-2.024a.668.668 0 0 1 .198-.471 1.575 1.575 0 1 0-2.228-2.228 3.818 3.818 0 0 0-1.12 2.687M6.9 7.575V12m6.27 4.318A4.49 4.49 0 0 1 16.35 15m.002 0h-.002" />
+</svg>
+
+          <>
+          <div className="flex flex-col gap-4">
+
+          
+               <span className="flex flex-row gap-4">
+               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 flex-shrink-0">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m11.25 11.25.041-.02a.75.75 0 0 1 1.063.852l-.708 2.836a.75.75 0 0 0 1.063.853l.041-.021M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Zm-9-3.75h.008v.008H12V8.25Z" />
+</svg>
+
+
+{action.info}
+</span>
+<span className="flex flex-row gap-4">
+
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M12 6v6h4.5m4.5 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
+</svg>
+
+{formatDateWithDotsWithTime(action.doneAt)}
+</span>  
+
+{action?.post?.name && 
+<span className="flex flex-row gap-4"> 
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 flex-shrink-0">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
+</svg>
+{action?.post?.name}
+</span>
+} 
+
+
+</div>       
+</>
+
+       </div>
+          ) ) 
+        ) : (
+          <p className="text-sm mt-2 text-gray-500  whitespace-nowrap ">Tento uživatel nevykonal žádný admin úkon.</p>
+        )}
+        </div>
+      </div>
+
+
+
+
+
+
+
+      </>
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
