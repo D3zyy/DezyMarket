38896f5b1a0f345ffc93962feb802741b361a98d
D3zyy <michaldolezel@gmail.com>
2025-01-27 16:15:49 +0100

    upd

--- app/admin/page.js
+++ app/admin/page.js
@@ -2,17 +2,53 @@ import React from 'react'
 import MenuByRole from './MenuByRole'
 import { getSession } from '../authentication/actions'
 import { redirect } from 'next/navigation';
-const Page = async () => {
-    const session = await getSession();
-    console.log(session)
+import { prisma } from '../database/db';
+import { select } from '@nextui-org/react';
 
+const Page = async () => {
+
+
+  let session,reports,suppTickets
+    
+
+    [session,reports,suppTickets] = await Promise.all([
+      getSession()
+      , prisma.postReport.findMany({
+        where: {
+          active: true,
+        },
+        include: {
+          post: {
+            select: {
+              id: true,
+              name: true,
+            },
+          },
+          user: {
+            select: {
+              id: true,
+              fullName: true,
+              role: {
+                select: {
+                  privileges: true,
+                },
+              },
+            },
+          },
+        },
+      }),
+      prisma.supportTickets.findMany({where: { active: true }, }),
+     
+    ]);
+
+    console.log(JSON.stringify(reports, null, 2)); // Pretty print with indentation
   if(!session || session?.role?.privileges <= 1|| !session.isLoggedIn || !session.email ){
       redirect('/');
   }
   return (
     <div>
       {session.role.privileges > 1 && 
-       <MenuByRole privileges={session.role.privileges} />
+       <MenuByRole supTick={suppTickets} reports={reports} privileges={session.role.privileges} />
       }
      
 
