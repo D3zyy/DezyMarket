26b3adddd78e17082431f1c549292179bb7b7ee2
D3zyy <michaldolezel@gmail.com>
2025-01-27 18:49:57 +0100

    upd

--- app/admin/MenuByRole.js
+++ app/admin/MenuByRole.js
@@ -72,12 +72,14 @@ function MenuByRole({supTick,reports,privileges}) {
     }, {})
   ).map((group, index) => (
     <div key={index} className="flex flex-col space-y-4 bg-base-200 p-4 rounded-lg shadow-sm">
+            <div className="badge badge-lg border-red-500 text-red-500 font-bold">Report</div>
       <div className="flex items-center space-x-4">
+
       <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-600 flex-shrink-0" >
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
 </svg>
 
-       
+      
         <Link
           href={`/post/${group.post.id}`}  // Odkaz na příspěvek
           className="underline hover:underline"
@@ -90,18 +92,12 @@ function MenuByRole({supTick,reports,privileges}) {
         <button
   disabled={loading}
   onClick={() => setDone(group.post.id, 'report')}
-  className="btn-sm btn btn-neutral break-all"
+  className="btn-sm btn bg-slate-300 dark:bg-neutral break-all"
 >
-  <svg
-    xmlns="http://www.w3.org/2000/svg"
-    fill="none"
-    viewBox="0 0 24 24"
-    strokeWidth={1.5}
-    stroke="currentColor"
-    className="size-6"
-  >
-    <path strokeLinecap="round" strokeLinejoin="round" d="M9 12.75 11.25 15 15 9.75M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z" />
-  </svg>
+<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="M15.042 21.672 13.684 16.6m0 0-2.51 2.225.569-9.47 5.227 7.917-3.286-.672ZM12 2.25V4.5m5.834.166-1.591 1.591M20.25 10.5H18M7.757 14.743l-1.59 1.59M6 10.5H3.75m4.007-4.243-1.59-1.59" />
+</svg>
+
   <span className="hidden sm:inline">Vyřešeno</span>
 </button>
 
