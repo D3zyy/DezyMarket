69d72d47190ae04eac9b1f539f5c818096a6d0da
D3zyy <michaldolezel@gmail.com>
2025-01-27 16:51:57 +0100

    upd

--- app/admin/MenuByRole.js
+++ app/admin/MenuByRole.js
@@ -27,7 +27,7 @@ function MenuByRole({supTick,reports,privileges}) {
         return <div>Chyby</div>;
       case "Tickets":
           return<>  
-          <div className="space-y-4 w-2/4">
+          <div className="space-y-4 w-2/4 min-w-72">
   {Object.values(
     allReports.reduce((groups, report) => {
       const postId = report.postId;  // Sesbíráme reporty podle postId
@@ -55,7 +55,7 @@ function MenuByRole({supTick,reports,privileges}) {
   ).map((group, index) => (
     <div key={index} className="flex flex-col space-y-4 bg-base-200 p-4 rounded-lg shadow-sm">
       <div className="flex items-center space-x-4">
-      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-600">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 text-gray-600 flex-shrink-0" >
   <path strokeLinecap="round" strokeLinejoin="round" d="M15.666 3.888A2.25 2.25 0 0 0 13.5 2.25h-3c-1.03 0-1.9.693-2.166 1.638m7.332 0c.055.194.084.4.084.612v0a.75.75 0 0 1-.75.75H9a.75.75 0 0 1-.75-.75v0c0-.212.03-.418.084-.612m7.332 0c.646.049 1.288.11 1.927.184 1.1.128 1.907 1.077 1.907 2.185V19.5a2.25 2.25 0 0 1-2.25 2.25H6.75A2.25 2.25 0 0 1 4.5 19.5V6.257c0-1.108.806-2.057 1.907-2.185a48.208 48.208 0 0 1 1.927-.184" />
 </svg>
 
@@ -79,7 +79,7 @@ function MenuByRole({supTick,reports,privileges}) {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="size-6 text-gray-600"
+                className="size-6 text-gray-600 flex-shrink-0"
               >
                 <path
                   strokeLinecap="round"
