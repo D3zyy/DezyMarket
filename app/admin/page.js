a755e5f0804a8995ea540a1598d6f6ee0b5efb74
D3zyy <michaldolezel@gmail.com>
2025-01-29 14:59:50 +0100

    upd

--- app/admin/page.js
+++ app/admin/page.js
@@ -4,14 +4,22 @@ import { getSession } from '../authentication/actions'
 import { redirect } from 'next/navigation';
 import { prisma } from '../database/db';
 import { getTypeOfAccountDetails } from '../typeOfAccount/Methods';
-
+import { DateTime } from 'luxon';
 const Page = async () => {
+  const now = DateTime.now().setZone('Europe/Prague');
+  const today = now.startOf('day');
+  const yesterday = today.minus({ days: 1 });
+  const startOfThisMonth = now.startOf('month');
+  const endOfThisMonth = now.endOf('month');
+  const startOfLastMonth = startOfThisMonth.minus({ months: 1 });
+  const endOfLastMonth = startOfThisMonth.minus({ milliseconds: 1 });
 
 
-  let session,reports,suppTickets,subscTypes,allTops
+  let session,reports,suppTickets,subscTypes,allTops,countOfAllUSers,registredTodayNumberOfUsr,registredYestrdayNumberOfUsr,registredThisMonthyNumberOfUsr,registredLastMonthyNumberOfUsr
+
     
 
-    [session,reports,suppTickets,subscTypes,allTops] = await Promise.all([
+    [session,reports,suppTickets,subscTypes,allTops,countOfAllUSers,registredTodayNumberOfUsr,registredYestrdayNumberOfUsr,registredThisMonthyNumberOfUsr,registredLastMonthyNumberOfUsr] = await Promise.all([
       getSession()
       , prisma.postReport.findMany({
         where: {
@@ -40,8 +48,49 @@ const Page = async () => {
       prisma.supportTickets.findMany({where: { active: true }, include : {ipOfusrOnsup : true} }),
       getTypeOfAccountDetails(),
        prisma.tops.findMany({}),
-    ]);
+       prisma.users.count(),
 
+        await prisma.users.count({
+        where: {
+          dateOfRegistration: {
+                gte: today.toISO(), // Od začátku dne
+                lt: today.plus({ days: 1 }).toISO() // Do konce dne
+            }
+        }
+    })
+,
+     await prisma.users.count({
+        where: {
+            dateOfRegistration: {
+                gte: yesterday.toISO(), // Od začátku včerejšího dne
+                lt: today.toISO() // Do konce včerejšího dne
+            }
+        }
+    }) , await prisma.users.count({
+      where: {
+        dateOfRegistration: {
+              gte: startOfThisMonth.toISO(),
+              lt: endOfThisMonth.toISO()
+          }
+      }
+  }), await prisma.users.count({
+      where: {
+        dateOfRegistration: {
+              gte: startOfLastMonth.toISO(),
+              lt: endOfLastMonth.toISO()
+          }
+      }
+  })
+
+    ]);
+    let usersStats = {}
+    usersStats.numberOfAllUsers = countOfAllUSers;
+    usersStats.numberOfRegistredUsrToday = registredTodayNumberOfUsr
+    usersStats.numberOfRegistredUsrYestrday = registredYestrdayNumberOfUsr
+    usersStats.numberOfRegistredUsrThisMonth = registredThisMonthyNumberOfUsr
+    usersStats.numberOfRegistredUsrLastMonth = registredLastMonthyNumberOfUsr
+
+    console.log("Statistiky:",usersStats);
     //console.log("all tops:",allTops)
   if(!session || session?.role?.privileges <= 1|| !session.isLoggedIn || !session.email ){
       redirect('/');
