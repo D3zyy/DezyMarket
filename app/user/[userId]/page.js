a44158ce80eaec92f74b818a5e1974169310f55c
D3zyy <michaldolezel@gmail.com>
2025-02-02 21:03:42 +0100

    upd

--- app/user/[userId]/page.js
+++ app/user/[userId]/page.js
@@ -321,7 +321,7 @@ return (
   <span>Registrace {formatDateWithDotsWithoutTime(userAcc?.dateOfRegistration)}</span>
 </div>
 
-{session.showCards && userAcc.id ===  session.userId &&<> 
+{ userAcc.id ===  session.userId &&<> 
 <div className="flex flex-row gap-6 ml-3 mt-3 items-center ">
 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                         <path strokeLinecap="round" strokeLinejoin="round" d="M2.25 8.25h19.5M2.25 9h19.5m-16.5 5.25h6m-6 2.25h3m-3.75 3h15a2.25 2.25 0 0 0 2.25-2.25V6.75A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25v10.5A2.25 2.25 0 0 0 4.5 19.5Z" />
