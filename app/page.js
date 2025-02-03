5ad26d7ff77a0af8d9bef269997fe07211e198da
D3zyy <michaldolezel@gmail.com>
2025-02-03 18:36:15 +0100

    upd

--- app/page.js
+++ app/page.js
@@ -36,8 +36,8 @@ const Page = async () => {
         </span>
       </div>
 
-      <div  className={`  flex justify-center font-bold    p-2 rounded-lg  max-w-[600px] mx-auto mt-2 mb-2 `}>
-      <label className="input input-bordered flex items-center gap-2">
+      <div  className={`  flex justify-center font-bold sticky md:static top-0   p-2 rounded-lg  max-w-[600px] mx-auto mt-2 mb-2 `}>
+      <label className="input input-bordered flex items-center  gap-2">
       <svg
     xmlns="http://www.w3.org/2000/svg"
     viewBox="0 0 16 16"
@@ -48,7 +48,7 @@ const Page = async () => {
       d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
       clipRule="evenodd" />
   </svg>
-  <input type="text" className="grow" placeholder="Vyhledat.." />
+  <input type="text" className="grow " placeholder="Vyhledat.." />
   
 </label>
       </div>
