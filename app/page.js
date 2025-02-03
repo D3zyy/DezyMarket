3df8b6df018d5d783033db364c117ccf6c33dddc
D3zyy <michaldolezel@gmail.com>
2025-02-03 20:38:58 +0100

    upd

--- app/page.js
+++ app/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { prisma } from './database/db';
 import localFont from 'next/font/local';
 import Link from 'next/link';
-
+import SearchComponent from './components/SearchComponent';
 const pacifico = localFont({
   src: '../public/fonts/Pacifico/Pacifico-Regular.ttf', // Začíná lomítkem
   weight: '400',
@@ -14,6 +14,7 @@ const bebas = localFont({
   weight: '400',
   style: 'normal',
 });
+
 const Page = async () => {
  
   
@@ -24,6 +25,9 @@ const Page = async () => {
         sections: true,
       },
     });
+
+
+
 
     return (<>         
       <div  className={`${bebas.className} flex justify-center text-4xl font-bold   p-2  `}>
@@ -38,20 +42,8 @@ const Page = async () => {
       </div>
 
       <div  className={`  flex justify-center font-bold sticky md:static mdtop-0   top-4  p-2 rounded-lg  max-w-[600px] mx-auto  mt-2  mb-2 `}>
-      <label className="input input-bordered flex items-center  gap-2">
-      <svg
-    xmlns="http://www.w3.org/2000/svg"
-    viewBox="0 0 16 16"
-    fill="currentColor"
-    className="h-4 w-4 opacity-70">
-    <path
-      fillRule="evenodd"
-      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-      clipRule="evenodd" />
-  </svg>
-  <input type="text" className="grow " placeholder="Vyhledat.." />
-  
-</label>
+      <SearchComponent/>
+     
       </div>
 
           <div className="flex justify-center items-center mx-auto border-2 border-dotted max-w-[300px]  md:max-w-[800px] border-gray-900  rounded-md p-4 ">
