ddaae394266e07613b5ac4832bbdc01d0e8af147
D3zyy <michaldolezel@gmail.com>
2025-02-03 18:16:33 +0100

    upd

--- app/page.js
+++ app/page.js
@@ -36,12 +36,29 @@ const Page = async () => {
         </span>
       </div>
 
+      <div  className={`  flex justify-center font-bold    p-2 rounded-lg  max-w-[600px] mx-auto mt-2 mb-2 `}>
+      <label className="input input-bordered flex items-center gap-2">
+      <svg
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 16 16"
+    fill="currentColor"
+    className="h-4 w-4 opacity-70">
+    <path
+      fillRule="evenodd"
+      d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+      clipRule="evenodd" />
+  </svg>
+  <input type="text" className="grow" placeholder="Vyhledat.." />
+  
+</label>
+      </div>
+
           <div className="flex justify-center items-center mx-auto border-2 border-dotted max-w-[300px]  md:max-w-[800px] border-gray-900  rounded-md p-4 ">
             <div className="grid grid-cols-1 md:grid-cols-4  gap-4 ml-14">
               {categories.map((category) => (
                 <div key={category.id} className="p-2  rounded-md ">
-                  <h2 className="text-lg font-bold underline flex  gap-2 ">
-                    {category.name} 
+                  <h2 className="text-lg font-bold  flex  gap-2 ">
+                  <span className='underline'> {category.name} </span> 
                     <span dangerouslySetInnerHTML={{ __html: category.logo }}></span>
                   </h2>
                   <ul className="">
