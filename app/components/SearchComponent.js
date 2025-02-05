a49a0196167a9717390d8a97721918c8e31945c0
D3zyy <michaldolezel@gmail.com>
2025-02-05 16:03:03 +0100

    upd

--- app/components/SearchComponent.js
+++ app/components/SearchComponent.js
@@ -6,15 +6,17 @@ function SearchComponent() {
   const [isLoadingSearch, setIsLoadingSearch] = useState(false); 
   const [foundData, setFoundData] = useState([]); 
   const searchTimeout = useRef(null); // useRef pro zajištění, že timeout je uchován mezi renderováními
+  const [searchQuery, setSearchQuery] = useState(""); // Uchovávat hledaný text
 
   const searchUser = async (info) => {
-    if(info.length > 0) {
+    setIsLoadingSearch(true); // Nastavení loading stavu na true
+    if(info.length >= 2) {
       if (searchTimeout.current) {
         clearTimeout(searchTimeout.current); // Pokud je aktivní timeout, zrušíme ho
       }
-
+      setFoundData('')
       searchTimeout.current = setTimeout(async () => { // Spustí se až po určitém zpoždění
-        setIsLoadingSearch(true); // Nastavení loading stavu na true
+       
         try {
           const response = await fetch('/api/search', {
             method: 'POST',
@@ -57,38 +59,42 @@ function SearchComponent() {
           />
         </svg>
         <input
-  type="text"
-  className="grow"
-  onChange={(e) => {
-    const value = e.target.value;
-    if (value.length >= 2) {
-      searchUser(value);
-    }
-  }}
-  placeholder="Vyhledat.."
-/>
+          type="text"
+          className="grow"
+          onChange={(e) => {
+            const value = e.target.value;
+            setSearchQuery(value); // Uložení hledaného textu
+            if (value.length >= 2) {
+              searchUser(value);
+            } else {
+              setFoundData([]); // Pokud je searchQuery kratší než 2 znaky, vyčistíme výsledky
+            }
+          }}
+          placeholder="Vyhledat.."
+        />
       </label>
 
       {/* Dropdown pro výsledky */}
       {isLoadingSearch ? (
-        <div className="absolute left-0 bg-base-300 right-0 shadow-md mt-1">
-          <p className=" flex p-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
-  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
-</svg>
-Hledám...</p>
-        </div>
+      <></>
       ) : (
-        foundData.length > 0 && (
-          <div className="absolute left-0 right-0 bg-base-300   shadow-md mt-1 max-h-60 overflow-y-auto">
-            {foundData?.map((post, index) => (
-              <Link  href={'/'}>
-             
-              <div key={index} className="p-2 hover:bg-base-200">
-                <h3 dangerouslySetInnerHTML={{ __html: post.name }} className="font-semibold" />
-                <p dangerouslySetInnerHTML={{ __html: post.description }} className="text-sm text-gray-600" />
-              </div>
+        foundData.length > 0 ? (
+          <div className="absolute left-0 right-0 bg-base-300 shadow-md mt-1 max-h-60 overflow-y-auto">
+            {foundData.map((post, index) => (
+              <Link key={index} href={`/post/${post.id}`}>
+                <div className="p-2 hover:bg-base-200">
+                  <h3 dangerouslySetInnerHTML={{ __html: post.name }} className="font-semibold" />
+                  <p dangerouslySetInnerHTML={{ __html: post.description }} className="text-sm text-gray-600" />
+                </div>
               </Link>
             ))}
+          </div>
+        ) : !isLoadingSearch &&searchQuery.length >= 2 &&  (
+          <div className="absolute left-0 right-0 bg-base-300 shadow-md mt-1 p-2 text-gray-500">
+            <p className='flex gap-2'><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6 flex-shrink-0">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM13.5 10.5h-6" />
+</svg>
+ Nenalezeno</p>
           </div>
         )
       )}
