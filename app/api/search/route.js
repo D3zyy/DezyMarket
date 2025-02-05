c945572c65f71acb7fa298daaedb35655d2d9c04
D3zyy <michaldolezel@gmail.com>
2025-02-05 15:19:55 +0100

    upd

--- app/api/search/route.js
+++ app/api/search/route.js
@@ -4,7 +4,7 @@ import { prisma } from "@/app/database/db";
 import { DateTime } from "luxon";
 
 export async function POST(req) {
-  let data, session;
+  let data;
   try {
     // Přečteme JSON data z požadavku
     data = await req.json();
@@ -15,97 +15,43 @@ export async function POST(req) {
       );
     }
 
-    console.log("Hledám:", data.searchQuery);
-
-    // Měření času pro full-textové vyhledávání
-    console.time('fullTextSearchTime'); // Začínáme měření
+    // Full-textové vyhledávání
     const foundPostsFullText = await prisma.posts.findMany({
-     
-      where: {
-        name: {
-          search: `${data.searchQuery}*`,
-        },
-        description: {
-          search: `${data.searchQuery}*`,
-        },
-      },
-    });
-    console.timeEnd('fullTextSearchTime'); // Konec měření
-
-    // Měření času pro LIKE vyhledávání
-    const likeSearchTimeLabel = 'likeSearchTime_' + Date.now(); // unikátní label pro LIKE
-    console.time(likeSearchTimeLabel); // Začínáme měření
-    const foundPostsLike = await prisma.posts.findMany({
       where: {
         OR: [
-          {
-            name: {
-              contains: data.searchQuery, // LIKE equivalent pro název
-            },
-          },
-          {
-            description: {
-              contains: data.searchQuery, // LIKE equivalent pro popis
-            },
-          },
+          { name: { search: `+${data.searchQuery}*` } },
+          { description: { search: `+${data.searchQuery}*` } },
         ],
       },
     });
-    console.timeEnd(likeSearchTimeLabel); // Konec měření pro LIKE
 
-    // Výpis výsledků a časů
-    console.log("=============================================");
-    console.log("Výsledky full-textového vyhledávání:");
-    console.log(`Počet nalezených příspěvků: ${foundPostsFullText.length}`);
-    console.log("=============================================");
-    console.log("Výsledky LIKE vyhledávání:");
-    console.log(`Počet nalezených příspěvků: ${foundPostsLike.length}`);
-    console.log("=============================================");
+    // Pro každý příspěvek vrátíme, jaké slovo odpovídá vyhledávacímu dotazu
+    const highlightedPosts = foundPostsFullText.map(post => {
+      const highlightText = (text) => {
+        const regex = new RegExp(data.searchQuery, "gi");
+        return text.replace(regex, (match) => `<span style="font-weight: 900; color: gray;">${match}</span>`);
+      };
 
-    return new Response(JSON.stringify({ 
-      dataFullText: foundPostsFullText, 
-      dataLike: foundPostsLike 
-    }), {
+      return {
+        ...post,
+        name: highlightText(post.name),
+        description: highlightText(post.description),
+        id: highlightText(post.id),
+      };
+    });
+
+    return new Response(JSON.stringify({ data: highlightedPosts }), {
       status: 200,
       headers: { "Content-Type": "application/json" },
     });
 
   } catch (error) {
-    try {
-      session = await getSession();
-      const rawIp =
-        req.headers.get("x-forwarded-for")?.split(",")[0] || 
-        req.headers.get("x-real-ip") || 
-        req.socket?.remoteAddress || 
-        null;
-
-      const ip = rawIp?.startsWith("::ffff:") ? rawIp.replace("::ffff:", "") : rawIp;
-
-      const dateAndTime = DateTime.now()
-        .setZone('Europe/Prague')
-        .toFormat("yyyy-MM-dd'T'HH:mm:ss'+00:00'");
-
-      await prisma.errors.create({
-        data: {
-          info: `Chyba na /api/search - POST - (catch) data: ${data} `,
-          dateAndTime: dateAndTime,
-          errorPrinted: error,
-          userId: session?.userId,
-          ipAddress: ip,
-        },
-      });
-    } catch (error) {}
-    console.log(error);
+    console.error(error);
     return new NextResponse(
-      JSON.stringify({
-        message: 'Chyba na serveru [POST] požadavek informace o předplatném',
-      }),
-      {
-        status: 500,
-        headers: { 'Content-Type': 'application/json' },
-      }
+      JSON.stringify({ message: 'Chyba na serveru.' }),
+      { status: 500 }
     );
   } finally {
-    await prisma.$disconnect(); // Uzavřete připojení po dokončení
+    await prisma.$disconnect();
   }
 }
--- app/components/SearchComponent.js
+++ app/components/SearchComponent.js
@@ -1,72 +1,94 @@
 "use client"
-import React from 'react'
-import { useState ,useRef} from 'react'
+import Link from 'next/link';
+import React, { useState, useRef } from 'react'
 
 function SearchComponent() {
-
   const [isLoadingSearch, setIsLoadingSearch] = useState(false); 
   const [foundData, setFoundData] = useState([]); 
- const searchTimeout = useRef(null); // useRef pro zajištění, že timeout je uchován mezi renderováními
+  const searchTimeout = useRef(null); // useRef pro zajištění, že timeout je uchován mezi renderováními
 
+  const searchUser = async (info) => {
+    if(info.length > 0) {
+      if (searchTimeout.current) {
+        clearTimeout(searchTimeout.current); // Pokud je aktivní timeout, zrušíme ho
+      }
 
+      searchTimeout.current = setTimeout(async () => { // Spustí se až po určitém zpoždění
+        setIsLoadingSearch(true); // Nastavení loading stavu na true
+        try {
+          const response = await fetch('/api/search', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+              searchQuery: info,
+            }),
+          });
 
-const searchUser = async (info) => {
-  if(info.length   > 0){
-
- 
-  if (searchTimeout.current) {
-    clearTimeout(searchTimeout.current); // Pokud je aktivní timeout, zrušíme ho
-  }
-
-  searchTimeout.current = setTimeout(async () => { // Spustí se až po určitém zpoždění
-    setIsLoadingSearch(true); // Nastavení loading stavu na true
-    try {
-      
-      const response = await fetch('/api/search', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({
-          searchQuery: info,
-        }),
-      });
-
-      const result = await response.json();
-      setFoundUsers(result?.users);
-     
-
-    } catch (error) {
-      console.error('Chyba získávání uživatele', error);
-    } finally {
-      setIsLoadingSearch(false); // Po dokončení nebo chybě, nastavíme loading na false
+          const result = await response.json();
+          
+          // Ujistíme se, že result je pole
+          setFoundData(Array.isArray(result.data) ? result.data : []);
+        } catch (error) {
+          console.error('Chyba získávání uživatele', error);
+        } finally {
+          setIsLoadingSearch(false); // Po dokončení nebo chybě, nastavíme loading na false
+        }
+      }, 1000); // 500 ms zpoždění
+    } else {
+      setFoundData([]); // Pokud je searchQuery prázdné, resetujeme výsledky
     }
-  }, 1000); // 500 ms zpoždění
-} else {
- 
-}
-};
-
-
-
-
+  };
 
   return (
-    <label className="input input-bordered flex items-center  gap-2">
-    <svg
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 16 16"
-  fill="currentColor"
-  className="h-4 w-4 opacity-70">
-  <path
-    fillRule="evenodd"
-    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-    clipRule="evenodd" />
-</svg>
-<input type="text" className="grow " onChange={(e) => (searchUser(e.target.value))} placeholder="Vyhledat.." />
+    <div className="relative">
+      <label className="input input-bordered flex items-center gap-2">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 16 16"
+          fill="currentColor"
+          className="h-4 w-4 opacity-70"
+        >
+          <path
+            fillRule="evenodd"
+            d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
+            clipRule="evenodd"
+          />
+        </svg>
+        <input 
+          type="text" 
+          className="grow" 
+          onChange={(e) => searchUser(e.target.value)} 
+          placeholder="Vyhledat.." 
+        />
+      </label>
 
-</label>
-  )
+      {/* Dropdown pro výsledky */}
+      {isLoadingSearch ? (
+        <div className="absolute left-0 bg-base-300 right-0 shadow-md mt-1">
+          <p className=" flex p-2"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+  <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+</svg>
+Hledám...</p>
+        </div>
+      ) : (
+        foundData.length > 0 && (
+          <div className="absolute left-0 right-0 bg-base-300   shadow-md mt-1 max-h-60 overflow-y-auto">
+            {foundData?.map((post, index) => (
+              <Link  href={'/'}>
+             
+              <div key={index} className="p-2 hover:bg-base-200">
+                <h3 dangerouslySetInnerHTML={{ __html: post.name }} className="font-semibold" />
+                <p dangerouslySetInnerHTML={{ __html: post.description }} className="text-sm text-gray-600" />
+              </div>
+              </Link>
+            ))}
+          </div>
+        )
+      )}
+    </div>
+  );
 }
 
-export default SearchComponent
+export default SearchComponent;
