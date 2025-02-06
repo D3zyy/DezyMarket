ed1b6f63602353c2f808dc48081dd7bfd55849a1
D3zyy <michaldolezel@gmail.com>
2025-02-06 20:41:25 +0100

    upd

--- app/search/[searchData]/layout.js
+++ app/search/[searchData]/layout.js
@@ -1,22 +1,44 @@
 import React from 'react'
 import SearchComponent from '@/app/components/SearchComponent'
+import localFont from 'next/font/local';
+
+// Správné cesty k fontům (musí být v /public/fonts/)
+const pacifico = localFont({
+    src: '/../../../public/fonts/Pacifico/Pacifico-Regular.ttf', // Začíná lomítkem
+    weight: '400',
+    style: 'normal',
+  });
+  
+  const bebas = localFont({
+    src: '/../../../public/fonts/Bebas_Neue/BebasNeue-Regular.ttf', // Začíná lomítkem
+    weight: '400',
+    style: 'normal',
+  });
 
 function Layout({ children }) {
-  
-    return (
+  return (
     <div>
-<div className="fixed top-20 left-1/2 transform -translate-x-1/2 flex justify-center items-center z-5  p-4">
-        <div className="join">
-          <div>
-            <SearchComponent />
-          </div>
-          <select className="select select-bordered join-item">
+      {/* Wrapper pro horní část */}
+      <div className="fixed top-10 left-1/2 transform -translate-x-1/2 flex flex-col items-center w-full z-10 p-4">
+        
+        {/* Nadpis */}
+        <h1 className={`${bebas.className} text-4xl font-bold`}>Dezy</h1>
+        
+        {/* Slogan */}
+        <span className={`${pacifico.className} text-lg font-semibold mt-1`}>
+          „Lepší místo pro vaše inzeráty.“
+        </span>
+
+        {/* Vyhledávání + kategorie */}
+        <div className="flex flex-wrap justify-center items-center gap-2 mt-4 w-full max-w-2xl">
+          <SearchComponent />
+          <select className="select select-bordered">
             <option disabled selected>Kategorie</option>
             <option>Sci-fi</option>
             <option>Drama</option>
             <option>Action</option>
           </select>
-          <select className="select select-bordered join-item">
+          <select className="select select-bordered">
             <option disabled selected>Sekce</option>
             <option>Sci-fi</option>
             <option>Drama</option>
@@ -25,9 +47,10 @@ function Layout({ children }) {
         </div>
       </div>
 
-      {children}
+      {/* Obsah stránky */}
+      <div className="mt-32">{children}</div>
     </div>
   )
 }
 
-export default Layout
+export default Layout;
