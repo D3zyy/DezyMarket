f9a30e6f0cb051a6372ada9a70f47e2f4f2b8a2c
D3zyy <michaldolezel@gmail.com>
2025-02-09 18:27:15 +0100

    upd

--- app/components/PostsPage.js
+++ app/components/PostsPage.js
@@ -6,7 +6,7 @@ import Post from '../search/Post';
 const PostsPage = () => {
     const [posts, setPosts] = useState([]);
     const [page, setPage] = useState(1);
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
     const [hasMore, setHasMore] = useState(true); // Určujeme, jestli jsou ještě další příspěvky
     const loaderRef = useRef(null);
     const searchParams = useSearchParams();
@@ -97,14 +97,16 @@ const PostsPage = () => {
                 posts.map((post) => (
                     <Post key={post.id} postDetails={post} section={post.section} />
                 ))
-            ) : (
-               
+            ) : (<> 
+                {!loading&&
                 <div className="flex gap-2 items-center justify-center w-full col-span-full text-center text-gray-500">
                 <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
                     <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607ZM13.5 10.5h-6" />
                 </svg>
                 Žádné příspěvky nebyly nalezeny
             </div>
+           }
+            </>
             )}
 
             {/* Loader */}
