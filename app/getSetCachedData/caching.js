504af753f46ff16571c8026ca36d582e698aeeb1
D3zyy <michaldolezel@gmail.com>
2025-02-12 16:00:04 +0100

    upd

--- app/getSetCachedData/caching.js
+++ app/getSetCachedData/caching.js
@@ -1,15 +1,21 @@
 import redis from "../redis/rd";
 
 export async function getCachedData(key, fetchFunction, ttl) {
+  console.time("Cache Operation Duration"); // Začátek měření času
+
   const cachedData = await redis.get(key);
+  let result;
 
   if (cachedData) {
-    console.log("CACHE HIT")
-    return JSON.parse(cachedData);
+    console.log("CACHE HIT");
+    result = JSON.parse(cachedData);
+  } else {
+    console.log("CACHE MISS");
+    // Pokud není v cache, zavolá fetchFunction
+    result = await fetchFunction();
+    await redis.setex(key, ttl, JSON.stringify(result)); // Uloží do cache s TTL
   }
-  console.log("CACHE MISS")
-  // Pokud není v cache, zavolá fetchFunction
-  const data = await fetchFunction();
-  await redis.setex(key, ttl, JSON.stringify(data)); // Uloží do cache s TTL
-  return data;
+
+  console.timeEnd("Cache Operation Duration"); // Konec měření času
+  return result;
 }
