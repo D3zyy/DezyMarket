8fbdf7258111b160ce6d3761d9683c2d3848098c
D3zyy <michaldolezel@gmail.com>
2025-02-15 14:42:37 +0100

    upd

--- app/termsOfUse/page.js
+++ app/termsOfUse/page.js
@@ -1,14 +1,147 @@
-"use client"
-
-
 function TermsOfUse() {
+    return (
+      <div className="flex flex-col container mx-auto p-4">
+        <h1 className="text-2xl font-bold mb-4">Podmínky použití webové stránky</h1>
+        
+        <div className="mb-4">
+          <h2 className="font-semibold">1. Úvodní ustanovení</h2>
+          <p>
+            Tento dokument obsahuje podmínky použití internetového inzertního portálu Dezy (dále jen „portál“), který je provozován fyzickou osobou Michal Doležel, IČO: 21992711 (dále jen „provozovatel“). Používáním portálu souhlasíte s těmito podmínkami.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">2. Účel portálu</h2>
+          <p>
+            Portál slouží jako online nástěnka pro inzerování a prodávání zboží mezi uživateli. Provozovatel neodpovídá za obsah inzerátů a není zodpovědný za obchodní transakce mezi inzerenty. Tento portál není obchodní platformou a jeho hlavním účelem je umožnit spojení mezi inzerentem a poptávajícím.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">3. Registrace a účet uživatele</h2>
+          <ul className="list-disc pl-6">
+            <li>Každý uživatel si může vytvořit svůj vlastní účet, který si vytvoří registrací na portálu. Minimální věk pro registraci je 18 let.</li>
+            <li>Uživatel je odpovědný za správnost a pravdivost údajů uvedených při registraci a za bezpečnost svého účtu.</li>
+            <li>Uživatel nesmí sdílet svůj účet s jinými osobami a nesmí používat účet jiného uživatele.</li>
+            <li>Každý uživatel může mít pouze jeden účet.</li>
+            <li>Provozovatel si vyhrazuje právo účet uživatele kdykoli zrušit, pozastavit nebo upravit, a to z jakéhokoli důvodu, včetně porušení těchto podmínek.</li>
+            <li>Uživatelé jsou povinni chránit své přihlašovací údaje a nesmí je zpřístupnit třetím stranám. V případě ztráty nebo zneužití přihlašovacích údajů je uživatel povinen okamžitě informovat provozovatele.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">4. Podmínky inzerce</h2>
+          <ul className="list-disc pl-6">
+            <li>Inzerce je povolena pouze na zboží, které je legální a odpovídá platným právním předpisům České republiky.</li>
+            <li>Zakázána je inzerce zboží, které je nelegální, nebezpečné, nebo porušuje autorská práva. Také je zakázáno inzerovat služby nebo produkty, které mohou ohrozit bezpečnost nebo zdraví uživatelů.</li>
+            <li>Inzeráty nesmí obsahovat klamavé, zavádějící, nebo nepravdivé informace.</li>
+            <li>Provozovatel si vyhrazuje právo na odstranění jakýchkoli inzerátů, které porušují podmínky nebo jsou v rozporu s právními předpisy.</li>
+            <li>Uživatelé nesmí provozovat na portálu žádnou podnikatelskou činnost, a to jak legální, tak nelegální. Tento portál není určen pro komerční prodeje, a to ani pro firmy, ani pro jednotlivce.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">5. Autorská práva k fotografiím</h2>
+          <ul className="list-disc pl-6">
+            <li>Uživatelé, kteří na portál nahrávají fotografie k inzerátům, souhlasí s tím, že tímto přenášejí veškerá autorská práva k těmto fotografiím na provozovatele portálu.</li>
+            <li>Uživatelé tímto prohlašují, že jsou držiteli autorských práv k nahrávaným fotografiím a že fotografie neporušují práva třetích stran.</li>
+            <li>Uživatelé nesmí nahrávat fotografie, které nemají právo používat, a nesmí porušovat autorská práva nebo jiné duševní vlastnictví třetích osob.</li>
+            <li>Provozovatel si vyhrazuje právo použít, upravit a šířit fotografie nahrané uživateli v souvislosti s propagací portálu.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">6. Zákaz použití API</h2>
+          <ul className="list-disc pl-6">
+            <li>Použití API portálu Dezy je výslovně zakázáno.</li>
+            <li>Uživatelé nesmí přistupovat k portálu nebo jeho obsahu automatizovanými prostředky, jako jsou skripty, roboty, nebo jiné nástroje, které by mohly poškodit nebo narušit fungování portálu.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">7. Zodpovědnost uživatelů</h2>
+          <ul className="list-disc pl-6">
+            <li>Uživatelé jsou odpovědní za veškerý obsah, který na portál umisťují, včetně textů, fotografií a dalších materiálů.</li>
+            <li>Uživatelé nesmí nahrávat obsah, který by mohl být považován za urážlivý, nelegální nebo jinak nevhodný.</li>
+            <li>Uživatelé jsou odpovědní za splnění všech právních povinností, které se vztahují k jejich inzerátům a transakcím. Provozovatel neprovádí žádné právní kontroly transakcí mezi uživateli.</li>
+          </ul>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">8. Zodpovědnost provozovatele</h2>
+          <ul className="list-disc pl-6">
+            <li>Provozovatel nenese odpovědnost za:</li>
+            <ul className="list-inside pl-6">
+              <li>Jakékoli ztráty, škody nebo zranění vzniklé z obchodních transakcí mezi uživateli.</li>
+              <li>Obsah inzerátů zveřejněných uživateli.</li>
+              <li>Jakékoli porušení právních předpisů uživateli.</li>
+              <li>Výpadky portálu nebo technické problémy, které mohou nastat při používání webu.</li>
+              <li>Uživatelé využívají portál na vlastní riziko.</li>
+            </ul>
+          </ul>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">9. Ochrana osobních údajů</h2>
+          <p>
+            Provozovatel respektuje ochranu osobních údajů uživatelů a nakládá s nimi v souladu s platnou legislativou (např. GDPR). Podrobnosti o ochraně osobních údajů jsou uvedeny v <a href="/privacy-policy" className="text-blue-500">zásadách ochrany osobních údajů</a>.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">10. Používání cookies a sledování</h2>
+          <p>
+            Portál používá soubory cookies a jiné technologie pro analýzu návštěvnosti a personalizaci obsahu. Používáním portálu souhlasíte s používáním cookies v souladu s našimi <a href="/cookies-policy" className="text-blue-500">zásadami používání cookies</a>.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">11. Omezení odpovědnosti za obsah</h2>
+          <p>
+            Provozovatel není odpovědný za žádné ztráty, škody nebo jiné následky, které vzniknou v souvislosti s použitím, neautorizovaným přístupem nebo zneužitím obsahu nahraného uživateli na portál. Uživatelé jsou odpovědní za obsah, který zveřejňují, a zaručují, že tento obsah neporušuje žádná práva třetích stran, včetně autorských práv, ochrany osobních údajů a dalších právních předpisů.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">12. Zneužívání portálu</h2>
+          <p>
+            Uživatelé nesmí zneužívat portál pro žádné nelegální, neetické nebo škodlivé aktivity, včetně (ale ne pouze) šíření malwaru, phishingu, spamu nebo jakékoli jinak škodlivé činnosti. Porušení těchto podmínek může vést k okamžitému zablokování účtu uživatele a právnímu postihu.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">13. Přenos práv a povinností</h2>
+          <p>
+            Provozovatel si vyhrazuje právo přenést veškerá práva a povinnosti vyplývající z těchto podmínek na třetí stranu, aniž by to mělo vliv na platnost těchto podmínek.
+          </p>
+        </div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">14. Vyhrazování práv k úpravám a ukončení přístupu</h2>
+          <p>
+            Provozovatel si vyhrazuje právo kdykoli inovovat nebo upravovat portál bez předchozího upozornění uživatelů. Portál nemusí být nepřetržitě dostupný, a to z důvodů údržby nebo jiných technických problémů.
+          </p>
+        </div>
+  
+       
+  
+        <div className="mb-4">
+  <h2 className="font-semibold">16. Řešení sporů</h2>
 
+  <p>
+    V případě sporu mezi uživatelem a provozovatelem, který se nepodaří vyřešit prostřednictvím naší <a className="text-blue-500" href="support">podpory</a>, má uživatel možnost využít mimosoudní řešení sporu prostřednictvím České obchodní inspekce . Pro více informací o mimosoudním řešení sporů a způsobu podání návrhu, navštivte jejich <a className="text-blue-500" href="https://coi.cz/mimosoudni-reseni-spotrebitelskych-sporu-adr/">stránky</a>.
+  </p>
 
-  return (
-    <div className="container mx-auto p-4">
-    yo podmínky
-    </div>
-  );
-}
-
-export default TermsOfUse;
+</div>
+  
+        <div className="mb-4">
+          <h2 className="font-semibold">17. Změna podmínek</h2>
+          <p>
+            Provozovatel si vyhrazuje právo kdykoli změnit, upravit nebo doplnit tyto podmínky. O změnách budou uživatelé informováni prostřednictvím e-mailu nebo oznámení na portálu Uživatel souhlasí s budoucími změnami těchto podmínek.
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
+  export default TermsOfUse;
