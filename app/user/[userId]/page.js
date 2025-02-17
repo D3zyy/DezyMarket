11f058b79aca34ec7e6a99f212baa511c9a7f5f7
D3zyy <michaldolezel@gmail.com>
2025-02-17 22:03:42 +0100

    upd

--- app/user/[userId]/page.js
+++ app/user/[userId]/page.js
@@ -896,7 +896,7 @@ Bude ukončen: {accType?.scheduleToCancel ? 'Ano' : 'Ne'}
   <path strokeLinecap="round" strokeLinejoin="round" d="M3 8.25V18a2.25 2.25 0 0 0 2.25 2.25h13.5A2.25 2.25 0 0 0 21 18V8.25m-18 0V6a2.25 2.25 0 0 1 2.25-2.25h13.5A2.25 2.25 0 0 1 21 6v2.25m-18 0h18M5.25 6h.008v.008H5.25V6ZM7.5 6h.008v.008H7.5V6Zm2.25 0h.008v.008H9.75V6Z" />
 </svg>
 
-{(posts.filter(post => post.visible).length > 0) || (posts.length > 0 && session?.role?.privileges > userAcc?.role?.privileges && session?.userId != userAcc?.id ) || session?.role?.privileges === 4 ? (
+{(posts.filter(post => post.visible).length > 0) || (posts.length > 0 && session?.role?.privileges > userAcc?.role?.privileges && session?.userId != userAcc?.id )? (
   <div
   className={`flex scrollbar-hidden 
     justify-center 
